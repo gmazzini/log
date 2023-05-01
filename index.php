988411bbb3907e8a1c4c82a8a36f77824577368f
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-01 19:19:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -51,7 +51,8 @@ else {
   echo "<input type=\"submit\" name=\"run\" value=\"find dw\">";
   echo "<br>";
 
-  echo "<input type=\"submit\" name=\"run\" value=\"import\">";
+  echo "<input type=\"submit\" name=\"run\" value=\"importadi\">";
+  echo "<input type=\"submit\" name=\"run\" value=\"importlzh\">";
   echo "<input type=\"submit\" name=\"run\" value=\"exportadi\">";
   echo "<input type=\"submit\" name=\"run\" value=\"exportcbr\">";
   echo "<input type=\"file\" name=\"myfile\">";
@@ -231,8 +232,37 @@ else {
       mysqli_free_result($query);
       break;
 
-    case "import";
-      if(isset($_FILES['myfile']['tmp_name']))$hh=fopen($_FILES['myfile']['tmp_name'],"r");
+    case "importlzh";
+      if(!isset($_FILES['myfile']['tmp_name']))break;
+      $hh=fopen($_FILES['myfile']['tmp_name'],"r");
+      $dateon="";
+      $freq="";
+      while(!feof($hh)){
+        $line=strtoupper(trim(fgets($hh)));
+        if(substr($line,0,1)=="D"){$dateon=substr($line,1); continue;}
+        if(substr($line,0,1)=="F"){$freqtx=substr($line,1)*1000; continue;}
+        if(substr($line,0,1)=="M"){$mode=substr($line,1); continue;}
+        $aux=eplode(" ",$line);
+        $timeon=$aux[0]."00";
+        $timeoff=$aux[0]."59";
+        $callsign=$aux[1];
+        $freqrx=$freqtx;
+        if($row[2]==null)$signaltx="59";
+        else $signaltx=$row[2];
+        if($row[3]==null)$signalrx="59";
+        else $signalrx=$row[3];
+        $dateoff=$dateon;
+        $start=substr($dateon,0,4)."-".substr($dateon,4,2)."-".substr($dateon,6,2)." ".substr($timeon,0,2).":".substr($timeon,2,2).":".substr($timeon,4,2);
+        $end=substr($dateoff,0,4)."-".substr($dateoff,4,2)."-".substr($dateoff,6,2)." ".substr($timeoff,0,2).":".substr($timeoff,2,2).":".substr($timeoff,4,2);
+        // mysqli_query($con,"insert into log (mycall,callsign,start,end,mode,freqtx,freqrx,signaltx,signalrx) value ('$mycall','$callsign','$start','$end','$mode',$freqtx,$freqrx,'$signaltx','$signalrx')");
+        echo "insert into log (mycall,callsign,start,end,mode,freqtx,freqrx,signaltx,signalrx) value ('$mycall','$callsign','$start','$end','$mode',$freqtx,$freqrx,'$signaltx','$signalrx')\n";
+      }
+      fclose($hh);
+      break;  
+      
+    case "importadi";
+      if(!isset($_FILES['myfile']['tmp_name']))break;
+      $hh=fopen($_FILES['myfile']['tmp_name'],"r");
       $aux="";
       while(!feof($hh)){
         $line=trim(fgets($hh));
