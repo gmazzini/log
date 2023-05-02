bbcb158393d4a33f020d323864fcd85a1ba1ded8
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-02 13:39:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -140,10 +140,10 @@ else {
   
   foreach($_POST['dxcsel'] as &$vv)$dxcsel[$vv]=1;
   foreach(array("160","80","60","40","30","20","17","15","12","10","PH","CW","DG") as &$vv){
-    echo "<label>$vv</label>";
     echo "<input type=\"checkbox\" name=\"dxcsel[]\" value=\"$vv\"";
     if($dxcsel[$vv])echo " checked";
     echo ">";
+    echo "<label>$vv</label>&nbsp;";
   }  
   echo "<br>";
   
