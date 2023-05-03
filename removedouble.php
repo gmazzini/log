4b0f63c4fd06a87a39c370b69f1b1b6f445adb4b
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-03 09:48:28 +0200

    Update removedouble.php

--- removedouble.php
+++ removedouble.php
@@ -1,8 +1,17 @@
 <?php
 include "local.php";
 
+$mycall="IK4LZH";
+
 $con=mysqli_connect("127.0.0.1",$dbuser,$dbpassword,$dbname);
 mysqli_query($con,"SET time_zone='+00:00'");
+
+$query=mysqli_query($con,"select call,start from log where mycall='$mycall'");
+for(;;){
+  $row=mysqli_fetch_array($query);
+  if($row==null)break;
+}
+
 
 
 
