761a16a5754156b9bfaee9da421aa82936664355
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-06 10:27:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -161,7 +161,7 @@ else {
           $query2=mysqli_query($con,"select count(start) from log where mycall='$mycall' and callsign='$row[0]'");
           $row2=mysqli_fetch_array($query2);
           mysqli_free_result($query2);
-          printf("%s %10s %7.1f %10s %03d %s\n",$row[3],$row[0],$row[2]/1000,$row[1],$row2[0],myqso($mycall,$row[0]));
+          printf("%s %10s %7.1f %10s %03d %s\n",$row[3],$row[0],$row[2]/1000,$row[1],$row2[0],myqso($con,$mycall,$row[0]));
           $myrow++;
           if($myrow>$mypage)break;
         }
