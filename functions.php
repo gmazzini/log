b3328300c7033e399fdf7ca6a5e61d090e82b6ee
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-06 07:59:56 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function myextract($buf,$token){
+  $pos=stripos($buf,"<".$token.":");
+  if($pos===false)return null;
+  $pose=stripos($buf,">",$pos);
+  $ltok=strlen($token)+2;
+  $ll=(int)substr($buf,$pos+$ltok,$pose-$pos-$ltok);
+  return substr($buf,$pose+1,$ll);
+}
+
+function myinsert($buf,$token){
+  return "<".$token.":".strlen($buf).">".$buf; 
+}
+
+?>
