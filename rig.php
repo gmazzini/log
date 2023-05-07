17f1bc7f795a1f7dc126874d8914e55a4350f697
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-07 11:36:44 +0200

    Update rig.php

--- rig.php
+++ rig.php
@@ -5,9 +5,9 @@ if($fp){
   stream_set_timeout($fp,0,200000);
   fwrite($fp,"fim\n");
   $line=trim(fgets($fp,30));
-  printf("%7.1f\n",(int)$line/1000);
+  printf("%.1f\n",(int)$line/1000);
   $line=trim(fgets($fp,30));
-  printf("%7.1f\n",(int)$line/1000);
+  printf("%.1f\n",(int)$line/1000);
   $line=trim(fgets($fp,30));
   printf("%s\n",$line);
 }
