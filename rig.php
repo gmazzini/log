5f0acff3bf993f557587deaed8af88e26f8b769c
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-07 10:17:15 +0200

    Update rig.php

--- rig.php
+++ rig.php
@@ -5,8 +5,7 @@ if($fp){
   stream_set_timeout($fp,0,200000);
   fwrite($fp,"fm\n");
   $line=trim(fgets($fp,30));
-  echo $line;
-printf("%7.1f\n",(int)$line/1000);
+  printf("%7.1f\n",(int)$line/1000);
   $line=fread($fp,30);
   echo $line;
 }
