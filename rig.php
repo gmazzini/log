17f09c98e983db7366eb93b573dc0a56cd153bfa
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-07 10:15:14 +0200

    Update rig.php

--- rig.php
+++ rig.php
@@ -4,9 +4,9 @@ $fp=fsockopen("188.209.85.89",6789);
 if($fp){
   stream_set_timeout($fp,0,200000);
   fwrite($fp,"fm\n");
-  $line=fgets($fp,30);
+  $line=trim(fgets($fp,30));
   echo $line;
-//  printf("%7,1f\n",((int)$line//100);
+printf("%7,1f\n",((int)$line/1000);
   $line=fread($fp,30);
   echo $line;
 }
