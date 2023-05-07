083772b6f8b6132c9bf5f82934cecc88a5ea4e05
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-07 10:15:49 +0200

    Update rig.php

--- rig.php
+++ rig.php
@@ -6,7 +6,7 @@ if($fp){
   fwrite($fp,"fm\n");
   $line=trim(fgets($fp,30));
   echo $line;
-printf("%7,1f\n",((int)$line/1000);
+printf("%7,1f\n",(int)$line/1000);
   $line=fread($fp,30);
   echo $line;
 }
