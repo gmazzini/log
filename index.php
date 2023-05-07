c282f87a23a2fea0abe8e2e745dd681a0ef53dc7
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-07 22:50:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -396,13 +396,14 @@ else {
           if(strlen($contesttx)==0)$contesttx=myextract($aux,"stx");
           $contestrx=myextract($aux,"srx_string");
           if(strlen($contestrx)==0)$contestrx=myextract($aux,"srx");
+          $contest=myextract($aux,"contest_id");
           $dateon=myextract($aux,"qso_date");
           $dateoff=myextract($aux,"qso_date_off");
           if(strlen($dateoff)==0)$dateoff=$dateon;
           $start=substr($dateon,0,4)."-".substr($dateon,4,2)."-".substr($dateon,6,2)." ".substr($timeon,0,2).":".substr($timeon,2,2).":".substr($timeon,4,2);
           $end=substr($dateoff,0,4)."-".substr($dateoff,4,2)."-".substr($dateoff,6,2)." ".substr($timeoff,0,2).":".substr($timeoff,2,2).":".substr($timeoff,4,2);
-          mysqli_query($con,"insert into log (mycall,callsign,start,end,mode,freqtx,freqrx,signaltx,signalrx,contesttx,contestrx,contest) value ('$mycall','$callsign','$start','$end','$mode',$freqtx,$freqrx,'$signaltx','$signalrx','$contesttx','$contestrx','')");
-          echo "('$mycall','$callsign','$start','$end','$mode',$freqtx,$freqrx,'$signaltx','$signalrx','$contesttx','$contestrx','')\n";
+          mysqli_query($con,"insert into log (mycall,callsign,start,end,mode,freqtx,freqrx,signaltx,signalrx,contesttx,contestrx,contest) value ('$mycall','$callsign','$start','$end','$mode',$freqtx,$freqrx,'$signaltx','$signalrx','$contesttx','$contestrx','$contest')");
+          echo "('$mycall','$callsign','$start','$end','$mode',$freqtx,$freqrx,'$signaltx','$signalrx','$contesttx','$contestrx','$contest')\n";
           $aux=substr($line,$pp+5);
         }
       }
