605983abaec6dc82d8562f044c0ef872a6f81349
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-08 09:24:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -174,7 +174,7 @@ else {
   echo "<h2>Frig: <span id=\"frig\"></span>&nbsp;";
   echo "Srig: <span id=\"srig\"></span>&nbsp;";
   echo "Mrig: <span id=\"mrig\"></span>&nbsp;";
-  echo "echo "<input type=\"submit\" name=\"run\" value=\"riglink\">&nbsp;";
+  echo "<input type=\"submit\" name=\"run\" value=\"riglink\">&nbsp;";
   echo "<br></h2>";  
   echo "<h1>$mycall $mygrid $page</h1>";
   switch($run){
