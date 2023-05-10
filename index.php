9166cebe2af39a63507a23300a0115e584f5fd4a
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-10 08:42:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -352,12 +352,13 @@ else {
     
     case "find":
       echo "<pre>";
-      $query=mysqli_query($con,"select start,callsign,freqtx,mode,signaltx,signalrx,lotw from log where callsign like '$Icallsign' and mycall='$mycall' order by start desc limit $mypage offset $page");
+      $query=mysqli_query($con,"select start,callsign,freqtx,mode,signaltx,signalrx,lotw,eqsl from log where callsign like '$Icallsign' and mycall='$mycall' order by start desc limit $mypage offset $page");
       for(;;){
         $row=mysqli_fetch_array($query);
         if($row==null)break;
         $aux="";
         if((int)$row[6]==1)$aux.="L";
+        if((int)$row[7]==1)$aux.="E";
         printf("%s %12s %7.1f %4s %5s %5s %2s\n",$row[0],$row[1],$row[2]/1000,$row[3],$row[4],$row[5],$aux);
       }
       echo "</pre>";
@@ -366,12 +367,13 @@ else {
 
     case "list";
       echo "<pre>";
-      $query=mysqli_query($con,"select start,callsign,freqtx,mode,signaltx,signalrx,lotw from log where mycall='$mycall' order by start desc limit $mypage offset $page");
+      $query=mysqli_query($con,"select start,callsign,freqtx,mode,signaltx,signalrx,lotw,eqsl from log where mycall='$mycall' order by start desc limit $mypage offset $page");
       for(;;){
         $row=mysqli_fetch_array($query);
         if($row==null)break;
         $aux="";
         if((int)$row[6]==1)$aux.="L";
+        if((int)$row[7]==1)$aux.="E";
         printf("%s %12s %7.1f %4s %5s %5s %2s\n",$row[0],$row[1],$row[2]/1000,$row[3],$row[4],$row[5],$aux);
       }
       echo "</pre>";
