848b447a29ecccc0d02e62ea69224c00fc9c1d85
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-10 19:32:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -493,11 +493,11 @@ else {
         else {
           $aux.=substr($line,0,$pp);
           $callsign=myextract($aux,"call");
-          $timeon=myextract($aux,"time_on").":00";
+          $timeon=myextract($aux,"time_on");
           $dateon=myextract($aux,"qso_date");
           $bb=substr($dateon,0,4)."-".substr($dateon,4,2)."-".substr($dateon,6,2)." ".substr($timeon,0,2).":".substr($timeon,2,2).":00";
-	        $ee=substr($dateon,0,4)."-".substr($dateon,4,2)."-".substr($dateon,6,2)." ".substr($timeon,0,2).":".substr($timeon,2,2).":59";
-	        $qsl=myextract($aux,"app_eqsl_ag");
+	  $ee=substr($dateon,0,4)."-".substr($dateon,4,2)."-".substr($dateon,6,2)." ".substr($timeon,0,2).":".substr($timeon,2,2).":59";
+	  $qsl=myextract($aux,"app_eqsl_ag");
           if($qsl=="Y"){
             echo "eqsl on $callsign $timeon $dateon\n";
             mysqli_query($con,"update log set eqsl=1 where mycall='$mycall' and callsign='$callsign' and start>='$bb' and start<='$ee'");
