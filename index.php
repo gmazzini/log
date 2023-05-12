5b921d7601c5e3ced2f587b084e0ec8282fc8cf4
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-12 07:03:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -207,7 +207,7 @@ else {
           if($freq>=$bpfreq[$i][1]&&$freq<$bpfreq[$i][2])break;
         }
         if(isset($bpfreq[$i])&&$dxcsel[$bpfreq[$i][0]]&&$dxcsel[$bpfreq[$i][3]]){
-          printf("%s %10s %7.1f %10s %s\n",$row[3],$row[0],$row[2]/1000,$row[1],myqso($con,$mycall,$row[0]));
+          printf("%s %12s %7.1f %10s %s\n",$row[3],$row[0],$row[2]/1000,$row[1],myqso($con,$mycall,$row[0]));
           $myrow++;
           if($myrow>$mypage)break;
         }
