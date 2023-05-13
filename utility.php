c663abd2c99b06df9ac942352ef5e8a4c7697610
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-13 08:23:05 +0200

    Update utility.php

--- utility.php
+++ utility.php
@@ -1,7 +1,7 @@
 <?php
 
 $myband=array(1=>160,3=>80,5=>60,7=>40,10=>30,14=>20,18=>17,21=>15,24=>12,28=>10,29=>10,50=>6,144=>2,145=>2);
-$mymode=array("SSB"=>"PH","CW"=>"CW","USB"=>"PH","LSB"=>"PH","FT8"=>"DG","RTTY"=>"DG","MFSK"=>"DG","FT4"=>"DG","FM"=>"PH","AM"=>"PH");
+$mymode=array("SSB"=>"PH","CW"=>"CW","USB"=>"PH","LSB"=>"PH","FT8"=>"DG","RTTY"=>"DG","MFSK"=>"DG","FT4"=>"DG","FM"=>"PH","AM"=>"PH","PKT"=>"DG","TOR"=>"DG","AMTOR"=>"DG");
 
 function myextract($buf,$token){
   $pos=stripos($buf,"<".$token.":");
