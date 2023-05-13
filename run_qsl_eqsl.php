afe7a2963afe72dd904104f1ab8c29985e1b0b86
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-13 12:16:16 +0200

    Create run_qsl_eqsl.php

---
+++ run_qsl_eqsl.php
@@ -0,0 +1,29 @@
+<?php
+
+if(!isset($_FILES['myfile']['tmp_name']))break;
+$hh=fopen($_FILES['myfile']['tmp_name'],"r");
+$aux="";
+echo "<pre>";
+while(!feof($hh)){
+  $line=trim(fgets($hh));
+  $pp=stripos($line,"<eor>");
+  if($pp===false)$aux.=$line;
+    else {
+      $aux.=substr($line,0,$pp);
+      $callsign=myextract($aux,"call");
+      $timeon=myextract($aux,"time_on");
+      $dateon=myextract($aux,"qso_date");
+      $bb=substr($dateon,0,4)."-".substr($dateon,4,2)."-".substr($dateon,6,2)." ".substr($timeon,0,2).":".substr($timeon,2,2).":00";
+	    $ee=substr($dateon,0,4)."-".substr($dateon,4,2)."-".substr($dateon,6,2)." ".substr($timeon,0,2).":".substr($timeon,2,2).":59";
+	    $qsl=myextract($aux,"app_eqsl_ag");
+      if($qsl=="Y"){
+        echo "qsl via eqsl on $callsign $dateon $timeon\n";
+        mysqli_query($con,"update log set eqsl=1 where mycall='$mycall' and callsign='$callsign' and start>='$bb' and start<='$ee'");
+      }
+      $aux=substr($line,$pp+5);
+   }
+}
+echo "</pre>";
+fclose($hh);
+
+?>
