cf4aa1a6e34c8b837e5c84de8c43cede54c3fee5
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-13 08:11:14 +0200

    Update run_report.php

--- run_report.php
+++ run_report.php
@@ -2,9 +2,21 @@
 
 echo "<pre>";
 
-$query=mysqli_query($con,"select count(lotw) from log where mycall='$mycall'");
-$row=mysqli_fetch_array($query);
-printf("# QSO: %6d\n",$row[0]);
+$query=mysqli_query($con,"select freqtx,mode,lotw,eqsl,qrz from log where mycall='$mycall' and lotw=1");
+for(;;){
+  $row=mysqli_fetch_array($query);
+  if($row==null)break;
+  $band=$myband[floor($row[0]/1000000)];
+  $mode=$mymode[$row[1]];
+  $cc[$band.$mode]++;
+  if($row[2]==1)$lotw[$band.$mode]++;
+  if($row[3]==1)$eqsl[$band.$mode]++;
+  if($row[4]==1)$qrz[$band.$mode]++;
+  $tot++;
+}
+ksort($cc);
+printf("%3d ",$tot);
+foreach($cc as $key=>$value)printf("%s %6d %6d %6d %6d\n",$value,$lotw[$key],$eqsl[$key],$qrz[$key]);
 mysqli_free_result($query);
 
 echo "</pre>";
