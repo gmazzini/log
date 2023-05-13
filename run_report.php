b0d90c31f1df841cf6fb8ec6888c8703b7636945
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-13 07:57:25 +0200

    Update run_report.php

--- run_report.php
+++ run_report.php
@@ -2,7 +2,12 @@
 
 echo "<pre>";
 
-echo "ciao\n";
+query=mysqli_query($con,"select count(lotw),count(eqsl),count(qrz) from log where mycall='$mycall'");
+$row=mysqli_fetch_array($query);
+printf("# lotw: %6d\n",$row[0]);
+printf("# eqsl: %6d\n",$row[1]);
+printf("# qrz: %6d\n",$row[2]);
+mysqli_free_result($query);
 
 echo "</pre>";
 
