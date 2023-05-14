dbb026b688a48040af2d7bbb5719af1837a8bb65
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-14 21:01:48 +0200

    Update run_report.php

--- run_report.php
+++ run_report.php
@@ -16,11 +16,12 @@ for(;;){
   $band=$myband[floor($row[0]/1000000)];
   $mode=$mymode[$row[1]];
   $tt=$band.$mode;
+  $dxcc=$row[5];
   myinc($w,0,$tt);
-  myinc($w,4,$row[5]);
-  if($row[2]==1)myinc($w,1,$tt);
-  if($row[3]==1)myinc($w,2,$tt);
-  if($row[4]==1)myinc($w,3,$tt);
+  myinc($w,4,$dxcc);
+  if($row[2]==1){myinc($w,1,$tt); myinc($w,5,$dxcc);}
+  if($row[3]==1){myinc($w,2,$tt); myinc($w,6,$dxcc);}
+  if($row[4]==1){myinc($w,3,$tt); myinc($w,7,$dxcc);}
   $tot++;
 }
 mysqli_free_result($query);
@@ -34,9 +35,10 @@ function mycmpkey($a,$b){
 }
 usort($key,mycmpkey);
 foreach($key as &$kk)printf("%10s %6d %6d %6d %6d\n",$kk,$w[0][$kk],$w[1][$kk],$w[2][$kk],$w[3][$kk]);
+echo "\n";
 
 $key=array_keys($w[4]);
-foreach($key as &$kk)printf("%10s %6d\n",$kk,$w[4][$kk]);
+foreach($key as &$kk)printf("%10s %6d %6d %6d %6d\n",$kk,$w[4][$kk],$w[5][$kk],$w[6][$kk],$w[7][$kk]);
 
 echo "</pre>";
 
