aacea7d6f8e2d9fa9cf33b858b1923604c27ea52
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-14 06:56:09 +0200

    Update run_start.php

--- run_start.php
+++ run_start.php
@@ -33,6 +33,21 @@ mysqli_free_result($query);
 echo "<pre>";
 printf("%s %s\n%s\n%s\n%s %s %s\n%s\n%s\n%s %s %s\n",$row[0],$row[1],$row[2],$row[3],$row[4],$row[5],$row[6],$row[7],$row[8],$row[9],$row[10],$row[11]);
 echo "\n";
+
+$query=mysqli_query($con,"select start,callsign,freqtx,mode,signaltx,signalrx,lotw,eqsl,qrz,contesttx,contestrx,contest from log where callsign='$Icallsign' and mycall='$mycall' order by start desc limit $mypage");
+for(;;){
+  $row=mysqli_fetch_array($query);
+  if($row==null)break;
+  $aux="";
+  if((int)$row[6]==1)$aux.="L";
+  if((int)$row[7]==1)$aux.="E";
+  if((int)$row[8]==1)$aux.="Q";
+  printf("%s %12s %7.1f %4s %5s %5s %-3s ",$row[0],$row[1],$row[2]/1000,$row[3],$row[4],$row[5],$aux);
+  if(strlen($row[11])>0)printf("(%s,%s,%s)",$row[11],$row[9],$row[10]);
+  printf("\n");
+}
+mysqli_free_result($query);
+
 echo myqso($con,$mycall,$Icallsign);
 echo "\n";
 $mys=findcall($Icallsign);
