ea3b2b893abb3753d5b6e4c13a670cc04817bc51
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-14 11:30:24 +0200

    Update run_report.php

--- run_report.php
+++ run_report.php
@@ -20,8 +20,7 @@ for(;;){
   if($row[2]==1)myinc($w,1,$tt);
   if($row[3]==1)myinc($w,2,$tt);
   if($row[4]==1)myinc($w,3,$tt);
-  $lookup=json_decode(findcall($row[5]));
-  print_r($lookup);
+  echo findcall($row[5]);
   exit(0);
   $tot++;
 }
