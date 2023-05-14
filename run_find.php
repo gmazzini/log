01612662ca87552a5e85b10a606a7f5779ee60ab
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-14 06:49:23 +0200

    Update run_find.php

--- run_find.php
+++ run_find.php
@@ -9,7 +9,7 @@ for(;;){
   if((int)$row[6]==1)$aux.="L";
   if((int)$row[7]==1)$aux.="E";
   if((int)$row[8]==1)$aux.="Q";
-  printf("%s %12s %7.1f %4s %5s %5s %-3s",$row[0],$row[1],$row[2]/1000,$row[3],$row[4],$row[5],$aux);
+  printf("%s %12s %7.1f %4s %5s %5s %-3s ",$row[0],$row[1],$row[2]/1000,$row[3],$row[4],$row[5],$aux);
   if(strlen($row[11])>0)printf("(%s,%s,%s)",$row[11],$row[9],$row[10]);
   printf("\n");
 }
