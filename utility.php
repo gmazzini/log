1ec0e03516721928a375d229f7d84ba037d284a1
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-16 22:18:08 +0200

    Update utility.php

--- utility.php
+++ utility.php
@@ -8,6 +8,13 @@ function myinc(&$w,$in,$el){
   else $w[$in][$el]=1;
 }
 
+function mycmpkey($a,$b){
+  if($a==$b)return 0;
+  $aa=((float)$a)*1000+ord(substr($a,strcspn($a,"CPD"),1));
+  $bb=((float)$b)*1000+ord(substr($b,strcspn($b,"CPD"),1));
+  return ($aa<$bb)?-1:1;
+}
+
 function mypost($token){
   global $_POST;
   if(isset($_POST[$token]))return $_POST[$token];
@@ -29,23 +36,24 @@ function myinsert($buf,$token){
 
 function myqso($con,$mycall,$callsign){
   global $myband,$mymode;
-  $tot=0;
+  unset($w);
   $query=mysqli_query($con,"select freqtx,mode,lotw,eqsl,qrz from log where mycall='$mycall' and callsign='$callsign'");
   for(;;){
     $row=mysqli_fetch_array($query);
     if($row==null)break;
     $band=$myband[floor($row[0]/1000000)];
     $mode=$mymode[$row[1]];
-    $cc[$band.$mode]++;
-    if($row[2]==1)$lotw[$band.$mode]++;
-    if($row[3]==1)$eqsl[$band.$mode]++;
-    if($row[4]==1)$qrz[$band.$mode]++;
-    $tot++;
+    $tt=$band.$mode;
+    myinc($w,0,$tt);
+    if($row[2]==1)myinc($w,1,$tt);
+    if($row[3]==1)myinc($w,2,$tt);
+    if($row[4]==1)myinc($w,3,$tt);
   }
-  ksort($cc);
-  $aux=sprintf("%3d ",$tot);
-  foreach($cc as $key=>$value)$aux.=$key."(".$value.",".$lotw[$key].",".$eqsl[$key].",".$qrz[$key].") ";
   mysqli_free_result($query);
+  $key=array_keys($w[0]);
+  usort($key,mycmpkey);
+  $aux=sprintf("%3d ",array_sum($w[0]));
+  foreach($key as &$kk)$aux.=$kk."(".$w[0][$kk].",".$w[1][$kk].",".$w[2][$kk].",".$w[3][$kk].") ";
   return $aux;
 }
 
