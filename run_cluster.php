9d2140ee172c421ecd3a58fcb338f6dd0c29d55c
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-16 21:35:11 +0200

    Update run_cluster.php

--- run_cluster.php
+++ run_cluster.php
@@ -13,7 +13,7 @@ for(;;){
   }
   if(isset($bpfreq[$i])&&$dxcsel[$bpfreq[$i][0]]&&$dxcsel[$bpfreq[$i][3]]){
     printf("%s %12s ",$row[3],$row[0]);
-    printf("<button type=\"button\" onclick=\"location.href='prova.php%s';\">%7.1f</button> ",$row[2],$freq);
+    printf("<button type=\"button\" onclick=\"myfreq(%s)\">%7.1f</button> ",$row[2],$freq);
     printf("%10s %s\n",$row[1],myqso($con,$mycall,$row[0]));
     $myrow++;
     if($myrow>$mypage)break;
