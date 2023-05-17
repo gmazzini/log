7f9b36edc6fc946d11b6e324e45251da141a5c81
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-17 16:38:06 +0200

    Update run_storcl.php

--- run_storcl.php
+++ run_storcl.php
@@ -2,8 +2,20 @@
 
 echo "<pre>";
 $ch=substr($run,-1);
-if(substr($run,0,3)=="sto")echo $Ifreq;
-echo "--$Ifreq---\n";
+if(substr($run,0,3)=="sto"){
+  if($riglink){
+    $frx=$_POST['Prigrx']*1000;
+    $ftx=$_POST['Prigtx']*1000;
+    if($ftx==0)$ftx=$frx;
+    $fmode=$_POST['Prigm'];
+  }
+  else {
+    $frx=$Ifreq*1000;
+    $ftx=$frx;
+    $fmode=$Imode;
+  }
+  file_put_contents("sto1.dat","$frx\n$ftx\n$mode\n");
+}
 
 echo $run;
 echo "\n";
