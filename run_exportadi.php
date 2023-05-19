a4fbbf090e70c7fd12bdbb4b7b16e0c33cd34a9d
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-19 21:46:10 +0200

    Update run_exportadi.php

--- run_exportadi.php
+++ run_exportadi.php
@@ -10,21 +10,21 @@ if(isset($_FILES['myfile']['tmp_name'])){
   $export_to=myextract($aux,"export_to");
   $query=mysqli_query($con,"select start,callsign,freqtx,mode,signaltx,signalrx,end,freqrx,contesttx,contestrx,contest from log where mycall='$mycall' and start>='$export_from' and start<='$export_to' order by start");
   for(;;){
-    $row=mysqli_fetch_array($query);
+    $row=mysqli_fetch_assoc($query);
     if($row==null)break;
-    fprintf($fp,"%s\n",myinsert($row[1],"CALL"));
-    fprintf($fp,"%s\n",myinsert(substr($row[0],0,4).substr($row[0],5,2).substr($row[0],8,2),"QSO_DATE"));
-    fprintf($fp,"%s\n",myinsert(substr($row[0],11,2).substr($row[0],14,2).substr($row[0],17,2),"TIME_ON"));
-    fprintf($fp,"%s\n",myinsert(substr($row[6],0,4).substr($row[6],5,2).substr($row[6],8,2),"QSO_DATE_OFF"));
-    fprintf($fp,"%s\n",myinsert(substr($row[6],11,2).substr($row[6],14,2).substr($row[6],17,2),"TIME_OFF"));
-    fprintf($fp,"%s\n",myinsert(sprintf("%7.5f",$row[2]/1000000),"FREQ"));
-    fprintf($fp,"%s\n",myinsert(sprintf("%7.5f",$row[7]/1000000),"FREQ_RX"));
-    fprintf($fp,"%s\n",myinsert($row[4],"RST_SENT"));
-    fprintf($fp,"%s\n",myinsert($row[5],"RST_RCVD"));
-    fprintf($fp,"%s\n",myinsert($row[3],"MODE")); 
-    fprintf($fp,"%s\n",myinsert($row[8],"STX_STRING"));
-    fprintf($fp,"%s\n",myinsert($row[9],"SRX_STRING"));
-    fprintf($fp,"%s\n",myinsert($row[10],"CONTEST_ID"));
+    fprintf($fp,"%s\n",myinsert($row["callsign"],"CALL"));
+    fprintf($fp,"%s\n",myinsert(substr($row["start"],0,4).substr($row["start"],5,2).substr($row["start"],8,2),"QSO_DATE"));
+    fprintf($fp,"%s\n",myinsert(substr($row["start"],11,2).substr($row["start"],14,2).substr($row["start"],17,2),"TIME_ON"));
+    fprintf($fp,"%s\n",myinsert(substr($row["end"],0,4).substr($row["end"],5,2).substr($row["end"],8,2),"QSO_DATE_OFF"));
+    fprintf($fp,"%s\n",myinsert(substr($row["end"],11,2).substr($row["end"],14,2).substr($row["end"],17,2),"TIME_OFF"));
+    fprintf($fp,"%s\n",myinsert(sprintf("%7.5f",$row["freqtx"]/1000000),"FREQ"));
+    fprintf($fp,"%s\n",myinsert(sprintf("%7.5f",$row["freqrx"]/1000000),"FREQ_RX"));
+    fprintf($fp,"%s\n",myinsert($row["signaltx"],"RST_SENT"));
+    fprintf($fp,"%s\n",myinsert($row["signalrx"],"RST_RCVD"));
+    fprintf($fp,"%s\n",myinsert($row["mode"],"MODE")); 
+    fprintf($fp,"%s\n",myinsert($row["contesttx"],"STX_STRING"));
+    fprintf($fp,"%s\n",myinsert($row["contestrx"],"SRX_STRING"));
+    fprintf($fp,"%s\n",myinsert($row["contest"],"CONTEST_ID"));
     fprintf($fp,"<EOR>\n\n");
   }
   fclose($fp);
