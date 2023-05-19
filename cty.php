8d7b9d8e040eba19017eea1e1ae2b6bdb805af7d
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-19 06:59:18 +0200

    Update cty.php

--- cty.php
+++ cty.php
@@ -102,4 +102,66 @@ function findcall($a){
   if($s!=-1)return $z1[$s];
 }
 
+function loadcty(){
+  $hh=fopen("cty.csv","r");
+  while(!feof($hh)){
+    $dd=fgetcsv($hh,100000);
+    if($dd===FALSE)continue;
+    $ee=explode(" ",substr($dd[9],0,-1));
+    foreach($ee as $ff){
+      $base=$dd[0];
+      $name=$dd[1];
+      $dxcc=$dd[2];
+      
+      $to1=strpos($ff,"{");
+      if($to1!==false){
+        $to2=strpos($ff,"}",$to1+1);
+        $cont=substr($ff,$to1+1,$to2-$to1-1);
+        $ff=substr($ff,0,$to1).substr($ff,$to2+1);
+      }
+      else $cont=$dd[3];
+      
+      $to1=strpos($ff,"(");
+      if($to1!==false){
+        $to2=strpos($ff,")",$to1+1);
+        $cqzone=(int)substr($ff,$to1+1,$to2-$to1-1);
+        $ff=substr($ff,0,$to1).substr($ff,$to2+1);
+      }
+      else $cqzone=(int)$dd[4];
+      
+      $to1=strpos($ff,"[");
+      if($to1!==false){
+        $to2=strpos($ff,"]",$to1+1);
+        $ituzone=(int)substr($ff,$to1+1,$to2-$to1-1);
+        $ff=substr($ff,0,$to1).substr($ff,$to2+1);
+      }
+      else $ituzone=(int)$dd[5];
+      
+      $to1=strpos($ff,"<");
+      if($to1!==false){
+        $to2=strpos($ff,">",$to1+1);
+        $to3=strpos($ff,"/",$to1+1);
+        $latitude=(float)substr($ff,$to1+1,$to3-$to1-1);
+        $longitude=(float)substr($ff,$to3+1,$to2-$to3-1);
+        $ff=substr($ff,0,$to1).substr($ff,$to2+1);
+      }
+      else {
+        $latitude=(float)$dd[6];
+        $longitude=(float)$dd[7];
+      }
+      
+      $to1=strpos($ff,"~");
+      if($to1!==false){
+        $to2=strpos($ff,"~",$to1+1);
+        $localtime=(float)substr($ff,$to1+1,$to2-$to1-1);
+        $ff=substr($ff,0,$to1).substr($ff,$to2+1);
+      }
+      else $localtime=(float)$dd[8];
+      
+      echo "$base $name $dxcc $cont $cqzobe $ituzone $latitude $longitude $localtime";
+    }
+  }
+  fclose($hh);
+}
+
 ?>
