52980a5f99f13a6e3c6b1baf2bfb37a8e1bcd3b7
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-19 21:35:35 +0200

    Update run_cluster.php

--- run_cluster.php
+++ run_cluster.php
@@ -4,16 +4,16 @@ echo "<pre>";
 $myrow=0;
 $query=mysqli_query($con,"select dx,spotter,freq,timespot from dxc order by timespot desc limit 1000");
 for(;;){
-  $row=mysqli_fetch_array($query);
+  $row=mysqli_fetch_assoc($query);
   if($row==null)break;
-  $freq=(float)$row[2]/1000;
-  $query2=mysqli_query($con,"select band,mode from bpfreq where $row[2]>=fromfreq and $row[2]<tofreq");
-  $row2=mysqli_fetch_array($query2);
+  $freq=(float)$row["freq"]/1000;
+  $query2=mysqli_query($con,"select band,mode from bpfreq where $row["freq"]>=fromfreq and $row["freq"]<tofreq");
+  $row2=mysqli_fetch_assoc($query2);
   mysqli_free_result($query2);
-  if(isset($dxcsel[$row2[0]])&&isset($dxcsel[$row2[1]])){
-    printf("%s %12s ",$row[3],$row[0]);
-    printf("<button type=\"button\" onclick=\"myfreqcall(%s,'%s')\">%7.1f</button> ",$row[2],$row[0],$freq);
-    printf("%10s %s\n",$row[1],myqso($con,$mycall,$row[0]));
+  if(isset($dxcsel[$row2["band"]])&&isset($dxcsel[$row2["mode"]])){
+    printf("%s %12s ",$row["timespot"],$row["dx"]);
+    printf("<button type=\"button\" onclick=\"myfreqcall(%s,'%s')\">%7.1f</button> ",$row["freq"],$row["dx"],$freq);
+    printf("%10s %s\n",$row[1],myqso($con,$mycall,$row["dx"]));
     $myrow++;
     if($myrow>$mypage)break;
   }
