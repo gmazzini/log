d02e16ade46478a89e615e6e1e1aca0827a770b6
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-19 22:03:15 +0200

    Update run_list.php

--- run_list.php
+++ run_list.php
@@ -3,14 +3,14 @@
 echo "<pre>";
 $query=mysqli_query($con,"select start,callsign,freqtx,mode,signaltx,signalrx,lotw,eqsl,qrz,contesttx,contestrx,contest from log where mycall='$mycall' order by start desc limit $mypage offset $page");
 for(;;){
-  $row=mysqli_fetch_array($query);
+  $row=mysqli_fetch_assoc($query);
   if($row==null)break;
   $aux="";
-  if((int)$row[6]==1)$aux.="L";
-  if((int)$row[7]==1)$aux.="E";
-  if((int)$row[8]==1)$aux.="Q";
-  printf("%s %12s %7.1f %4s %5s %5s %-3s ",$row[0],$row[1],$row[2]/1000,$row[3],$row[4],$row[5],$aux);
-  if(strlen($row[11])>0)printf("(%s,%s,%s)",$row[11],$row[9],$row[10]);
+  if((int)$row["lotw"]==1)$aux.="L";
+  if((int)$row["eqsl"]==1)$aux.="E";
+  if((int)$row["qrz"]==1)$aux.="Q";
+  printf("%s %12s %7.1f %4s %5s %5s %-3s ",$row["start"],$row["callsign"],$row["freqtx"]/1000,$row["mode"],$row["signaltx"],$row["signalrx"],$aux);
+  if(strlen($row["contest"])>0)printf("(%s,%s,%s)",$row["contest"],$row["contesttx"],$row["contestrx"]);
   printf("\n");
 }
 echo "</pre>";
