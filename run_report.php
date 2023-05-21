5488c0455980d795fa8b781944dbaa7c5850c602
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-21 08:45:17 +0200

    Update run_report.php

--- run_report.php
+++ run_report.php
@@ -46,7 +46,7 @@ arsort($w[4]);
 printf("<b>%10s %6d %6d %6d %6d</b>\n","dxcc",count($w[4]),count($w[5]),count($w[6]),count($w[7]));
 $key=array_keys($w[4]);
 foreach($key as &$kk){
-  $query=mysqli_query($con,"select name from cty where dxcc=$kk");
+  $query=mysqli_query($con,"select name from cty where dxcc=$kk limit 1");
   $row=mysqli_fetch_assoc($query);
   mysqli_free_result($query);
   printf("%10s %6d %6d %6d %6d %s\n",$kk,$w[4][$kk],$w[5][$kk],$w[6][$kk],$w[7][$kk],$row["name"]);
