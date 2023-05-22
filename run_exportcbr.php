5440618a3a3efc3867f7ef3b14c78a8a6bde3c7b
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-22 21:42:54 +0200

    Update run_exportcbr.php

--- run_exportcbr.php
+++ run_exportcbr.php
@@ -32,6 +32,7 @@ if(isset($_FILES['myfile']['tmp_name'])){
     fprintf($fp,"QSO: %5d %2s %04d-%02d-%02d ",$row["freqtx"]/1000,$mymode[$row["mode"]],substr($row["start"],0,4),substr($row["start"],5,2),substr($row["start"],8,2));
     fprintf($fp,"%02d%02d %-13s %3s %-6s %-13s %3s %-6s 0\n",substr($row["start"],11,2),substr($row["start"],14,2),$mycall,$row["signaltx"],$row["contesttx"],$row["callsign"],$row["signalrx"],$row["contestrx"]);
   }
+  mysqli_free_result($query);
   fprintf($fp,"END-OF-LOG:\n");
   fclose($fp);
   echo "<pre><a href='https://log.chaos.cc/files/$name' download>Download Cabrillo</a><br>";
