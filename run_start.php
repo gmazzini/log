1e27083fc7bc2010d7dc238a1e7b1dc43e13a11b
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-23 18:02:38 +0200

    Update run_start.php

--- run_start.php
+++ run_start.php
@@ -22,10 +22,11 @@ elseif($Icallsign=="USB"||$Icallsign=="LSB"||$Icallsign=="CW"){
 }
 else {
   $qsostart=gmdate('Y-m-d H:i:s');
-  $query=mysqli_query($con,"select firstname,lastname,addr1,addr2,state,zip,country,grid,email,cqzone,ituzone,born,image from who where callsign='$Icallsign'");
-  $row=mysqli_fetch_row($query);
+  $query=mysqli_query($con,"select firstname from who where callsign='$Icallsign'");
+  $row=mysqli_fetch_assoc($query);
+  mysqli_free_result($query);
   $ff=0;
-  if($row!=null&&strlen($row[0])==0){$ff=1; mysqli_query($con,"delete from who where callsign='$Icallsign'");}
+  if($row!=null&&strlen($row["firstname"])==0){$ff=1; mysqli_query($con,"delete from who where callsign='$Icallsign'");}
   if($row==null||$ff){
     $qrzkey=myrcl($con,"qrzkey");    
     $q1=file_get_contents("http://xmldata.qrz.com/xml/current/?s=$qrzkey;callsign=$Icallsign");
@@ -57,7 +58,6 @@ else {
       mysqli_query($con,"insert into who (callsign,firstname,lastname,addr1,addr2,state,zip,country,grid,email,cqzone,ituzone,born,image,myupdate) value ('$Icallsign','$row[0]','$row[1]','$row[2]','$row[3]','$row[4]','$row[5]','$row[6]','$row[7]','$row[8]',$row[9],$row[10],$row[11],'$row[12]','$mynow')");
     }
   }
-  mysqli_free_result($query);
   $query=mysqli_query($con,"select firstname,lastname,addr1,addr2,state,zip,country,grid,email,cqzone,ituzone,born,image from who where callsign='$Icallsign'");
   $row=mysqli_fetch_assoc($query);
   mysqli_free_result($query);
