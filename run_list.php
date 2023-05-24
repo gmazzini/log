c80a38e13a8819a5183d62f79c365ca9fdab161b
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-24 06:37:38 +0200

    Update run_list.php

--- run_list.php
+++ run_list.php
@@ -9,7 +9,7 @@ for(;;){
   if((int)$row["lotw"]==1)$aux.="L";
   if((int)$row["eqsl"]==1)$aux.="E";
   if((int)$row["qrz"]==1)$aux.="Q";
-  if((int)$row["freqrx"]>0&&(int)$row["freqrx"]!=(int)$row["freqtx"])$aux.=sprintf("[%.1f]",((int)$row["freqtx"]-(int)$row["freqrx"]/1000);
+  if((int)$row["freqrx"]>0&&(int)$row["freqrx"]!=(int)$row["freqtx"])$aux.=sprintf("[%.1f]",((int)$row["freqtx"]-(int)$row["freqrx"])/1000);
   printf("%s %12s %7.1f %4s %5s %5s %-3s ",$row["start"],$row["callsign"],$row["freqtx"]/1000,$row["mode"],$row["signaltx"],$row["signalrx"],$aux);
   if(strlen($row["contest"])>0)printf("(%s,%s,%s)",$row["contest"],$row["contesttx"],$row["contestrx"]);
   printf("\n");
