b11cdafbe34f8cc8039e57140d446d05bcd60a6e
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-26 07:09:26 +0200

    Update run_report.php

--- run_report.php
+++ run_report.php
@@ -36,6 +36,7 @@ for(;;){
   myinc($w,9,$tt,$callsign);
   myinc($w,10,$callsign);
   myinc($w,11,$tt,$wpx);
+  myinc($w,12,$wpx);
   if($row["lotw"]==1){myinc($w,1,$tt); myinc($w,5,$dxcc);}
   if($row["eqsl"]==1){myinc($w,2,$tt); myinc($w,6,$dxcc);}
   if($row["qrz"]==1){myinc($w,3,$tt); myinc($w,7,$dxcc);}
@@ -43,7 +44,7 @@ for(;;){
 mysqli_free_result($query);
 
 printf("<p id=\"myh1\">%10s %6s %8s %8s %8s %8s %8s</p>","Band/Mode","QSO","QSO.uniq","QSO.wpx","QSL.LOTW","QSL.EQSL","QSL.QRZ");
-printf("<p id=\"myh2\">%10s %6d %8d %8d %8d %8d %8d</p>","Tot",array_sum($w[0]),count($w[10]),0,array_sum($w[1]),array_sum($w[2]),array_sum($w[3]));
+printf("<p id=\"myh2\">%10s %6d %8d %8d %8d %8d %8d</p>","Tot",array_sum($w[0]),count($w[10]),count($w[12]),,array_sum($w[1]),array_sum($w[2]),array_sum($w[3]));
 $key=array_keys($w[0]);
 usort($key,"mycmpkey");
 foreach($key as &$kk)@printf("%10s %6d %8d %8d %8d %8d %8d\n",$kk,$w[0][$kk],count($w[9][$kk]),count($w[11][$kk]),$w[1][$kk],$w[2][$kk],$w[3][$kk]);
