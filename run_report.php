370b5deb7679d72de0213e2cbd76861de597b485
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-26 07:14:39 +0200

    Update run_report.php

--- run_report.php
+++ run_report.php
@@ -37,6 +37,7 @@ for(;;){
   myinc($w,10,$callsign);
   myinc($w,11,$tt,$wpx);
   myinc($w,12,$wpx);
+  myinc($w,13,$dxcc,$wpx);
   if($row["lotw"]==1){myinc($w,1,$tt); myinc($w,5,$dxcc);}
   if($row["eqsl"]==1){myinc($w,2,$tt); myinc($w,6,$dxcc);}
   if($row["qrz"]==1){myinc($w,3,$tt); myinc($w,7,$dxcc);}
@@ -51,14 +52,14 @@ foreach($key as &$kk)@printf("%10s %6d %8d %8d %8d %8d %8d\n",$kk,$w[0][$kk],cou
 echo "\n";
 
 arsort($w[4]);
-printf("<p id=\"myh1\">%10s %6s %8s %8s %8s %8s %s</p>","dxcc","QSO","QSO.uniq","QSL.LOTW","QSL.EQSL","QSL.QRZ","Country");
-printf("<p id=\"myh2\">%10s %6d %8d %8d %8d %8d</p>","Tot",count($w[4]),count($w[8]),count($w[5]),count($w[6]),count($w[7]));
+printf("<p id=\"myh1\">%10s %6s %8s %8s %8s %8s %8s %s</p>","dxcc","QSO","QSO.uniq","QSO.wpx","QSL.LOTW","QSL.EQSL","QSL.QRZ","Country");
+printf("<p id=\"myh2\">%10s %6d %8d %8d %8d %8d %8d</p>","Tot",count($w[4]),count($w[8]),count($w[13]),count($w[5]),count($w[6]),count($w[7]));
 $key=array_keys($w[4]);
 foreach($key as &$kk){
   $query=mysqli_query($con,"select name from cty where dxcc=$kk limit 1");
   $row=mysqli_fetch_assoc($query);
   mysqli_free_result($query);
-  @printf("%10s %6d %8d %8d %8d %8d %s\n",$kk,$w[4][$kk],count($w[8][$kk]),$w[5][$kk],$w[6][$kk],$w[7][$kk],$row["name"]);
+  @printf("%10s %6d %8d %8d %8d %8d %8d %s\n",$kk,$w[4][$kk],count($w[8][$kk]),count($w[13][$kk]),$w[5][$kk],$w[6][$kk],$w[7][$kk],$row["name"]);
 }
 
 echo "</pre>";
