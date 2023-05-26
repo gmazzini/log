cf705170d8fbdd738492d5cf6e3bf293eec4f415
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-26 22:32:20 +0200

    Create tool_proc.php

---
+++ tool_proc.php
@@ -0,0 +1,23 @@
+<?php
+include "local.php";
+$con=mysqli_connect("127.0.0.1",$dbuser,$dbpassword,$dbname);
+mysqli_query($con,"SET time_zone='+00:00'");
+$mycall="IK4LZH";
+$query=mysqli_query($con,"select act from booking where mycall='IK4LZH'");
+for(;;){
+  $row=mysqli_fetch_assoc($query);
+  if($row==null)break;
+  $act=$row["act"];
+  $cc=explode("|",$act);
+  switch($cc[0]){
+    case "D":
+      $start=$cc[1];
+      $callsign=$cc[2];
+      echo "delete from log where mycall='$mycall' and start='$start' and callsign='$callsign'";
+      break;
+  }
+}
+mysqli_free_result($query2);
+mysqli_close($con);
+
+?>
