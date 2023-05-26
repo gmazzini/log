0ae14cdb5e54daa05e3847bb77350e0e06e9e1c3
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-26 22:50:09 +0200

    Update tool_proc.php

--- tool_proc.php
+++ tool_proc.php
@@ -3,7 +3,8 @@ include "local.php";
 $con=mysqli_connect("127.0.0.1",$dbuser,$dbpassword,$dbname);
 mysqli_query($con,"SET time_zone='+00:00'");
 $mycall="IK4LZH";
-$query=mysqli_query($con,"select act,myupdate from booking where mycall='IK4LZH'");
+
+$query=mysqli_query($con,"select act,myupdate from booking where mycall='$mycall'");
 for(;;){
   $row=mysqli_fetch_assoc($query);
   if($row==null)break;
@@ -15,9 +16,10 @@ for(;;){
       $start=$cc[1];
       $callsign=$cc[2];
       echo "delete from log where mycall='$mycall' and start='$start' and callsign='$callsign'\n";
+      mysqli_query($con,"delete from log where mycall='$mycall' and start='$start' and callsign='$callsign'");
       break;
   }
-  mysqli_query($con,"delete from booking where mycall='IK4LZH' and act='$act' and myupdate='$myupdate'");
+  mysqli_query($con,"delete from booking where mycall='$mycall' and act='$act' and myupdate='$myupdate'");
 }
 mysqli_free_result($query);
 mysqli_close($con);
