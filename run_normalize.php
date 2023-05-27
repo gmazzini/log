03553d0a6205600f075676b8f08d96848106b258
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-27 07:09:11 +0200

    Update run_normalize.php

--- run_normalize.php
+++ run_normalize.php
@@ -19,6 +19,23 @@ for(;;){
 mysqli_free_result($query);
 echo "Set dxcc: $qq\n\n";
 
+$query=mysqli_query($con,"select command,myupdate from booking where mycall='$mycall' order by myupdate");
+for(;;){
+  $row=mysqli_fetch_assoc($query);
+  if($row==null)break;
+  $command=$row["command"];
+  $myupdate=$row["myupdate"];
+  $cc=explode(",",$command);
+  $para=$cc[1];
+  switch($cc[0]){
+    case "DEL":
+    case "DELETE":
+      $do="delete from log where mycall='$mycall' and ";
+      break;
+  }
+}
+mysqli_free_result($query);
+
 echo "</pre>";
 
 ?>
