9c0c76015486ef554cac473d3a829438bb93ba80
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-27 08:18:27 +0200

    Update run_report.php

--- run_report.php
+++ run_report.php
@@ -2,23 +2,6 @@
 
 echo "<pre>";
 
-$qq=0;
-$query=mysqli_query($con,"select start,callsign from log where mycall='$mycall' and dxcc=0");
-for(;;){
-  $row=mysqli_fetch_assoc($query);
-  if($row==null)break;
-  $aux=searchcty($con,$row["callsign"]);
-  if($aux!=null){
-    $dxcc=(int)$aux["dxcc"];
-    $mstart=$row["start"];
-    $mcallsign=$row["callsign"];
-    mysqli_query($con,"update log set dxcc=$dxcc where mycall='$mycall' and start='$mstart' and callsign='$mcallsign' and dxcc=0");
-    $qq++;
-  }
-}
-mysqli_free_result($query);
-echo "Set dxcc: $qq\n\n";
-
 unset($w);
 $query=mysqli_query($con,"select callsign,freqtx,mode,lotw,eqsl,qrz,dxcc from log where mycall='$mycall'");
 for(;;){
