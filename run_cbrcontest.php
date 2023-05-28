d250bbffd19eae73d368c61d97b990221256822e
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-28 22:21:11 +0200

    Update run_cbrcontest.php

--- run_cbrcontest.php
+++ run_cbrcontest.php
@@ -9,12 +9,13 @@ if(isset($_FILES['myfile']['tmp_name'])){
     if(substr($line,0,4)!="QSO:")continue;
     $parts=preg_split('/\s+/',$line);
     $start=$parts[3]." ".substr($parts[4],0,2).":".substr($parts[4],2,2).":00";
+    $end=$parts[3]." ".substr($parts[4],0,2).":".substr($parts[4],2,2).":59";
     $callsign=$parts[8];
     $contesttx=$parts[7];
     $contestrx=$parts[10];
     $contest=$Icontest;
-    echo "update log set contesttx='$contesttx',contestrx='$contestrx',contest='$contest' where mycall='$mycall' and callsign='$callsign' and start='$start'\n";
-    mysqli_query($con,"update log set contesttx='$contesttx',contestrx='$contestrx',contest='$contest' where mycall='$mycall' and callsign='$callsign' and start='$start'");
+    echo "update log set contesttx='$contesttx',contestrx='$contestrx',contest='$contest' where mycall='$mycall' and callsign='$callsign' and start>='$start' and start<='$end'\n";
+    mysqli_query($con,"update log set contesttx='$contesttx',contestrx='$contestrx',contest='$contest' where mycall='$mycall' and callsign='$callsign' and start>='$start' and start<='$end'");
   }
   echo "</pre>";
   fclose($hh);
