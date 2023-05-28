f62c36db4794170721498e2db8ab34f50158e9b7
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-28 10:09:22 +0200

    Update run_cluster.php

--- run_cluster.php
+++ run_cluster.php
@@ -13,7 +13,7 @@ for(;;){
   mysqli_free_result($query2);
   if(isset($dxcsel[$row2["band"]])&&isset($dxcsel[$row2["mode"]])){
     printf("%s %12s ",$row["timespot"],$row["dx"]);
-    printf("<button type=\"button\" onclick=\"myfreqcall(%s,'%s')\">%7.1f</button> ",$row["freq"],$row["dx"],$freq);
+    printf("<button type=\"button\" id=\"myb2\" onclick=\"myfreqcall(%s,'%s')\">%7.1f</button> ",$row["freq"],$row["dx"],$freq);
     printf("%10s %s\n",$row["spotter"],myqso($con,$mycall,$row["dx"]));
     $myrow++;
     if($myrow>$mypage)break;
