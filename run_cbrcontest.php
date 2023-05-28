56932513f48b03c4385bc0f9e5da309012484cdc
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-28 21:58:55 +0200

    Update run_cbrcontest.php

--- run_cbrcontest.php
+++ run_cbrcontest.php
@@ -6,7 +6,14 @@ if(isset($_FILES['myfile']['tmp_name'])){
   echo "<pre>";
   while(!feof($hh)){
     $line=trim(fgets($hh));
-   
+    if(substr($line,0,4)!="QSO:")continue;
+    $parts=mysep($line,10);
+    $start=$parts[3]." ".substr($parts[4],0,2).":".substr($parts[4],2,2)":00";
+    $callsign=$parts[8];
+    $contesttx=$part[7];
+    $contestrx=$parts[10];
+    $contest=$Icontest;
+    echo "update log set contesttx='$contesttx',contestrx='$contestrx',contest='$contest' where mycall='$mycall' and callsign='$callsign' and start='$start'\n";
   }
   echo "</pre>";
   fclose($hh);
