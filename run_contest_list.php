f12d2f3a6b32005f56d594b7859311b6c1d4fc52
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-29 07:01:29 +0200

    Update run_contest_list.php

--- run_contest_list.php
+++ run_contest_list.php
@@ -6,9 +6,9 @@ for(;;){
   $row=mysqli_fetch_assoc($query);
   if($row==null)break;
   $contest=$row["contest"];
-  $query2=mysqli_query($con,"select min(start),max(start) from log where mycall='$mycall' and contest='$contest'");
+  $query2=mysqli_query($con,"select min(start),max(start),count(callsign) from log where mycall='$mycall' and contest='$contest'");
   $row2=mysqli_fetch_row($query2);
-  printf("%20s: %s -> %s\n",$contest,$row2[0],$row2[1]);
+  printf("%20s: [%4d] %s -> %s\n",$contest,$row2[2],$row2[0],$row2[1]);
   mysqli_free_result($query2);
 }
 mysqli_free_result($query);
