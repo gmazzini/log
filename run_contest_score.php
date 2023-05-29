db72caf9ef7fe8bf306eb1e287278553e7265c7d
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-29 07:16:59 +0200

    Update run_contest_score.php

--- run_contest_score.php
+++ run_contest_score.php
@@ -1,11 +1,16 @@
 <?php
 
 $ll={
-  {"ARIDX","https://ik4lzh.mazzini.org/aridx.php"}
+  {"ARIDX","https://ik4lzh.mazzini.org/aridx.php"},
+  {"CQWWSSB","https://ik4lzh.mazzini.org/cqww.php"},
+  {"CQWWCW","https://ik4lzh.mazzini.org/cqww.php"}
 };
 
 echo "<pre>";
 echo "$Icontest\n";
+foreach($ll as $v){
+  echo "$v[0]\n";
+}
 echo "</pre>";
 
 ?>
