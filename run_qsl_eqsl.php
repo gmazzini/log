a9023ef293c0edd25cefe9a682b8aedb9b448cc9
Gianluca Mazzini <gianluca@mazzini.org>
2023-06-02 06:58:39 +0200

    Update run_qsl_eqsl.php

--- run_qsl_eqsl.php
+++ run_qsl_eqsl.php
@@ -13,8 +13,10 @@ if(isset($_FILES['myfile']['tmp_name'])){
       $callsign=myextract($aux,"call");
       $timeon=myextract($aux,"time_on");
       $dateon=myextract($aux,"qso_date");
-      $bb=substr($dateon,0,4)."-".substr($dateon,4,2)."-".substr($dateon,6,2)." ".substr($timeon,0,2).":".substr($timeon,2,2).":00";
-      $ee=substr($dateon,0,4)."-".substr($dateon,4,2)."-".substr($dateon,6,2)." ".substr($timeon,0,2).":".substr($timeon,2,2).":59";
+      $xx=substr($dateon,0,4)."-".substr($dateon,4,2)."-".substr($dateon,6,2)." ".substr($timeon,0,2).":".substr($timeon,2,2).":00";
+      $xss=strtotime($ss);
+      $bb=date("Y-m-d H:i:s",$xss-$qslwin);
+      $ee=date("Y-m-d H:i:s",$xss+$qslwin);
       $qsl=myextract($aux,"app_eqsl_ag");
       if($qsl=="Y"){
         echo "qsl via eqsl on $callsign $dateon $timeon\n";
