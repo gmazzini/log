07265abcc18fe8c48a4f94f305415a86258081be
Gianluca Mazzini <gianluca@mazzini.org>
2023-06-02 09:09:05 +0200

    Update run_start.php

--- run_start.php
+++ run_start.php
@@ -87,6 +87,12 @@ else {
   myprint($mys);
   echo "\n\n";
   
+  $dxcc=$mys["dxcc"];
+  $query=mysqli_query($con,"select count(callsign) from log where mycall='$mycall' and dxcc=$dxcc");
+  $row=mysqli_fetch_assoc($query);
+  mysqli_free_result($query);
+  echo "Same dxcc: $row[0]\n\n";
+  
   $mydbt=dbt($con,$mycall,$Icallsign);
   myprint($mydbt);
   echo "\n\n";
