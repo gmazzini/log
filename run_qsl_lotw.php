24029c8b99687c8a7a65ebf120b1e8c560ad7e07
Gianluca Mazzini <gianluca@mazzini.org>
2023-06-03 05:56:25 +0200

    Update run_qsl_lotw.php

--- run_qsl_lotw.php
+++ run_qsl_lotw.php
@@ -13,13 +13,13 @@ if(isset($_FILES['myfile']['tmp_name'])){
       $callsign=myextract($aux,"call");
       $timeon=myextract($aux,"time_on");
       $dateon=myextract($aux,"qso_date");
-      $xx=substr($dateon,0,4)."-".substr($dateon,4,2)."-".substr($dateon,6,2)." ".substr($timeon,0,2).":".substr($timeon,2,2).":00";
+      $ss=substr($dateon,0,4)."-".substr($dateon,4,2)."-".substr($dateon,6,2)." ".substr($timeon,0,2).":".substr($timeon,2,2).":00";
       $xss=strtotime($ss);
       $bb=date("Y-m-d H:i:s",$xss-$qslwin);
       $ee=date("Y-m-d H:i:s",$xss+$qslwin);
       $qsl=myextract($aux,"qsl_rcvd");
       if($qsl=="Y"){
-        echo "qsl via lotw on $callsign $dateon $timeon\n";
+        echo "update log set lotw=1 where mycall='$mycall' and callsign='$callsign' and start>='$bb' and start<='$ee'\n";
         mysqli_query($con,"update log set lotw=1 where mycall='$mycall' and callsign='$callsign' and start>='$bb' and start<='$ee'");
       }
       $aux=substr($line,$pp+5);
