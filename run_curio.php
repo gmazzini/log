f9b21b710d9398c13ad3a387b9c8557d3796bd85
Gianluca Mazzini <gianluca@mazzini.org>
2023-06-04 08:26:20 +0200

    Update run_curio.php

--- run_curio.php
+++ run_curio.php
@@ -14,15 +14,15 @@ for(;;){
   myinc($w,2,$mode); $h[2]="mode";
   if($row["lotw"]==1){
     myinc($w,3,$callsign); $h[3]="call.lotw";
-    myinc($w,4,$dxcc); $h[4]="dxcc.lotw";
+    myinc($w,4,$dxcc); $h[4]="dxcc.lotw"; $v[4]=1;
   }
   if($row["eqsl"]==1){
     myinc($w,5,$callsign); $h[5]="call.eqsl";
-    myinc($w,6,$dxcc); $h[6]="dxcc.eqsl";
+    myinc($w,6,$dxcc); $h[6]="dxcc.eqsl"; $v[6]=1;
   }
   if($row["qrz"]==1){
     myinc($w,7,$callsign); $h[7]="call.qrz";
-    myinc($w,8,$dxcc); $h[8]="dxcc.qrz";
+    myinc($w,8,$dxcc); $h[8]="dxcc.qrz"; $v[8]=1;
   }
 }
 mysqli_free_result($query);
@@ -35,7 +35,14 @@ for($i=0;$i<=8;$i++){
   $cc=0;
   printf("<b>%s</b><br>",$h[$i]);
   foreach($w[$i] as $k => $v){
-    printf("%10s %6d<br>",$k,$v);
+    if((int)$v[$i]==1){
+      $query=mysqli_query($con,"select base from cty where dxcc=$k limit 1");
+      $row=mysqli_fetch_assoc($query);
+      $base=$row["base"];
+      mysqli_free_result($query);
+      printf("%s%s %6d<br>",$k,$base,$v);
+    }
+    else printf("%10s %6d<br>",$k,$v);
     $cc++;
     if($cc>30)break;
   }
