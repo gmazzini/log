c89a7d0c2dd66ef254b9feffb08710d8fb3a016d
Gianluca Mazzini <gianluca@mazzini.org>
2023-06-04 07:36:10 +0200

    Update run_curio.php

--- run_curio.php
+++ run_curio.php
@@ -9,12 +9,21 @@ for(;;){
   $mode=$mymode[$row["mode"]];
   $dxcc=$row["dxcc"];
   $callsign=$row["callsign"];
-  myinc($w,0,$callsign);
-  myinc($w,1,$band);
-  myinc($w,2,$mode);
-  if($row["lotw"]==1){myinc($w,3,$callsign);myinc($w,4,$dxcc);}
-  if($row["eqsl"]==1){myinc($w,5,$callsign);myinc($w,6,$dxcc);}
-  if($row["qrz"]==1){myinc($w,7,$callsign);myinc($w,8,$dxcc);}
+  myinc($w,0,$callsign); $h[0]="call";
+  myinc($w,1,$band); $h[1]="band";
+  myinc($w,2,$mode); $h[2]="mode";
+  if($row["lotw"]==1){
+    myinc($w,3,$callsign); $h[3]="call.lotw";
+    myinc($w,4,$dxcc); $h[4]="dxcc.lotw";
+  }
+  if($row["eqsl"]==1){
+    myinc($w,5,$callsign); $h[5]="call.eqsl";
+    myinc($w,6,$dxcc); $h[6]="dxcc.eqsl";
+  }
+  if($row["qrz"]==1){
+    myinc($w,7,$callsign); $h[7]="call.qrz";
+    myinc($w,8,$dxcc); $h[8]="dxcc.qrz";
+  }
 }
 mysqli_free_result($query);
 
@@ -24,6 +33,7 @@ for($i=0;$i<=8;$i++){
   echo "<td>";
   arsort($w[$i]);
   $cc=0;
+  printf("<b>%s</b>",$h[$i]);
   foreach($w[$i] as $k => $v){
     printf("%10s %6d<br>",$k,$v);
     $cc++;
