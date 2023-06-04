1f1bcdbd30b514367480236a7f1b2a98a498fdba
Gianluca Mazzini <gianluca@mazzini.org>
2023-06-04 09:08:33 +0200

    Update run_curio.php

--- run_curio.php
+++ run_curio.php
@@ -13,15 +13,15 @@ for(;;){
   myinc($w,1,$band); $h[1]="band"; $v[1]=0;
   myinc($w,2,$mode); $h[2]="mode"; $v[2]=0;
   if($row["lotw"]==1){
-    myinc($w,3,$callsign); $h[3]="call.lotw";
+    myinc($w,3,$callsign); $h[3]="call.lotw"; $v[3]=0;
     myinc($w,4,$dxcc); $h[4]="dxcc.lotw"; $v[4]=1;
   }
   if($row["eqsl"]==1){
-    myinc($w,5,$callsign); $h[5]="call.eqsl";
+    myinc($w,5,$callsign); $h[5]="call.eqsl"; $v[5]=0;
     myinc($w,6,$dxcc); $h[6]="dxcc.eqsl"; $v[6]=1;
   }
   if($row["qrz"]==1){
-    myinc($w,7,$callsign); $h[7]="call.qrz";
+    myinc($w,7,$callsign); $h[7]="call.qrz"; $v[7]=0;
     myinc($w,8,$dxcc); $h[8]="dxcc.qrz"; $v[8]=1;
   }
 }
@@ -35,7 +35,7 @@ for($i=0;$i<=8;$i++){
   $cc=0;
   printf("<b>%s</b><br>",$h[$i]);
   foreach($w[$i] as $k => $v){
-    if((int)$v[$i]==1){
+    if($v[$i]==1){
       $query=mysqli_query($con,"select base from cty where dxcc=$k limit 1");
       $row=mysqli_fetch_assoc($query);
       $base=$row["base"];
