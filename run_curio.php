bf280c071e5bbc8bb706380b0b8e06ab3a3641ec
Gianluca Mazzini <gianluca@mazzini.org>
2023-06-04 15:18:11 +0200

    Update run_curio.php

--- run_curio.php
+++ run_curio.php
@@ -33,7 +33,7 @@ for($i=0;$i<=8;$i++){
   echo "<td><pre>";
   arsort($w[$i]);
   $cc=0;
-  printf("<b>%s</b>\n",$h[$i]);
+  printf("<div style=\"text-align:center;\"><b>%s</b></div>\n",$h[$i]);
   foreach($w[$i] as $k => $v){
     if($p[$i]==1){
       $query=mysqli_query($con,"select base from cty where dxcc=$k limit 1");
