6ac9bc51634f53aaccefbef028a216da503664f0
Gianluca Mazzini <gianluca@mazzini.org>
2023-06-07 06:47:29 +0200

    Update run_start.php

--- run_start.php
+++ run_start.php
@@ -1,6 +1,7 @@
 <?php
 // also triggered by ENTER in Call input
 include "def_qrz.php";
+include "def_list.php";
 echo "<pre>";
 
 if(is_numeric($Icallsign)){
@@ -48,20 +49,7 @@ else {
   echo "</td>";
   printf("</table>\n");
 
-  $query=mysqli_query($con,"select start,callsign,freqtx,mode,signaltx,signalrx,lotw,eqsl,qrz,contesttx,contestrx,contest from log where callsign='$Icallsign' and mycall='$mycall' order by start desc limit 5");
-  for(;;){
-    $row=mysqli_fetch_assoc($query);
-    if($row==null)break;
-    $aux="";
-    if((int)$row["lotw"]==1)$aux.="L";
-    if((int)$row["eqsl"]==1)$aux.="E";
-    if((int)$row["qrz"]==1)$aux.="Q";
-    printf("%s %12s %7.1f %4s %5s %5s %-3s ",$row["start"],$row["callsign"],$row["freqtx"]/1000,$row["mode"],$row["signaltx"],$row["signalrx"],$aux);
-    if(strlen($row["contest"])>0)printf(" (%s,%s,%s)",$row["contest"],$row["contesttx"],$row["contestrx"]);
-    if((int)$row["freqrx"]>0&&(int)$row["freqrx"]!=(int)$row["freqtx"])printf(" [%+.1f]",((int)$row["freqrx"]-(int)$row["freqtx"])/1000);
-    printf("\n");
-  }
-  mysqli_free_result($query);
+  mylist($con,"where callsign='$Icallsign' and mycall='$mycall' order by start desc limit 5",$mycall,$md5passwd);  
   echo "\n";
 
   echo myqso($con,$mycall,$Icallsign);
