8b255d7ecf1737490cdcaffd32d96836590f78eb
Gianluca Mazzini <gianluca@mazzini.org>
2023-06-07 06:59:21 +0200

    Update def_list.php

--- def_list.php
+++ def_list.php
@@ -2,7 +2,7 @@
 
 function mylist($con,$condition,$mycall,$md5passwd){
   echo "<pre>";
-  $query=mysqli_query($con,"select start,callsign,freqtx,freqrx,mode,signaltx,signalrx,lotw,eqsl,qrz,contesttx,contestrx,contest from log $condition");
+  $query=mysqli_query($con,"select start,end,callsign,freqtx,freqrx,mode,signaltx,signalrx,lotw,eqsl,qrz,contesttx,contestrx,contest from log $condition");
   for(;;){
     $row=mysqli_fetch_assoc($query);
     if($row==null)break;
@@ -10,8 +10,15 @@ function mylist($con,$condition,$mycall,$md5passwd){
     if((int)$row["lotw"]==1)$aux.="L";
     if((int)$row["eqsl"]==1)$aux.="E";
     if((int)$row["qrz"]==1)$aux.="Q";
+    
+    $timediff=strtotime($row["end"])-strtotime($row["start"]);
+    if($timediff==0)$taux="00s";
+    elseif($timediff<60)$taux=sprintf("%2ds",$timediff);
+    elseif ($timediff<3600)$taux=sprintf("%2dm",$timediff/60);
+    elseif $taux=sprintf("%2dh",$timediff/3600);
+    
     printf("<button type=\"button\" id=\"myb2\" onclick=\"mycommand('%s','%s','%s','%s')\"> </button> ",$mycall,$md5passwd,$row["start"],$row["callsign"]);
-    printf("%s %12s %7.1f %4s %5s %5s %-3s ",$row["start"],$row["callsign"],$row["freqtx"]/1000,$row["mode"],$row["signaltx"],$row["signalrx"],$aux);
+    printf("%s&%s %12s %7.1f %4s %5s %5s %-3s ",$row["start"],$taux,$row["callsign"],$row["freqtx"]/1000,$row["mode"],$row["signaltx"],$row["signalrx"],$aux);
     if(strlen($row["contest"])>0)printf(" (%s,%s,%s)",$row["contest"],$row["contesttx"],$row["contestrx"]);
     if((int)$row["freqrx"]>0&&(int)$row["freqrx"]!=(int)$row["freqtx"])printf(" [%+.1f]",((int)$row["freqrx"]-(int)$row["freqtx"])/1000);
     printf("\n");
