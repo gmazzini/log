1ada73e9ee22673d824bbdd5494292a7981439e6
Gianluca Mazzini <gianluca@mazzini.org>
2023-06-08 07:06:08 +0200

    Update def_list.php

--- def_list.php
+++ def_list.php
@@ -12,13 +12,13 @@ function mylist($con,$condition,$mycall,$md5passwd){
     if((int)$row["qrz"]==1)$aux.="Q";
     
     $timediff=strtotime($row["end"])-strtotime($row["start"]);
-    if($timediff==0)$taux="00s";
-    elseif($timediff<60)$taux=sprintf("%2ds",$timediff);
-    elseif ($timediff<3600)$taux=sprintf("%2dm",$timediff/60);
-    else $taux=sprintf("%2dh",$timediff/3600);
+    if($timediff==0)$taux="(0s)";
+    elseif($timediff<60)$taux=sprintf("(%ds)",$timediff);
+    elseif ($timediff<3600)$taux=sprintf("(%dm)",$timediff/60);
+    else $taux=sprintf("(%dh)",$timediff/3600);
     
     printf("<button type=\"button\" id=\"myb2\" onclick=\"mycommand('%s','%s','%s','%s')\"> </button> ",$mycall,$md5passwd,$row["start"],$row["callsign"]);
-    printf("%s(%s) %12s %7.1f %4s %5s %5s %-3s ",$row["start"],$taux,$row["callsign"],$row["freqtx"]/1000,$row["mode"],$row["signaltx"],$row["signalrx"],$aux);
+    printf("%s%5s %12s %7.1f %4s %5s %5s %-3s ",$row["start"],$taux,$row["callsign"],$row["freqtx"]/1000,$row["mode"],$row["signaltx"],$row["signalrx"],$aux);
     if(strlen($row["contest"])>0)printf(" (%s,%s,%s)",$row["contest"],$row["contesttx"],$row["contestrx"]);
     if((int)$row["freqrx"]>0&&(int)$row["freqrx"]!=(int)$row["freqtx"])printf(" [%+.1f]",((int)$row["freqrx"]-(int)$row["freqtx"])/1000);
     printf("\n");
