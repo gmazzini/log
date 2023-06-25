020f1f49d1722bf09f40859759ecf10e33252dc1
Gianluca Mazzini <gianluca@mazzini.org>
2023-06-25 07:58:15 +0200

    Update run_activity.php

--- run_activity.php
+++ run_activity.php
@@ -20,22 +20,43 @@ for(;;){
   $dxcc=$row["dxcc"];
   $callsign=$row["callsign"];
   $wpx=wpx($callsign);
-  $year=substr($row["start"],0,4);
+  $start=$row["start"];
+  
+  $year=substr($start,0,4);
   myinc($w,0,$year);
   myinc($w,1,$year,$mode);
   myinc($w,2,$year,$dxcc);
   myinc($w,3,$year,$callsign);
   myinc($w,4,$year,$wpx);
-  if($row["lotw"]==1){myinc($w,5,$year);}
-  if($row["eqsl"]==1){myinc($w,6,$year);}
-  if($row["qrz"]==1){myinc($w,7,$year);}
+  if($row["lotw"]==1)myinc($w,5,$year);
+  if($row["eqsl"]==1)myinc($w,6,$year);
+  if($row["qrz"]==1)myinc($w,7,$year);
+  
+  $yymm=substr($start,0,7);
+  if($yymm>=$ei2&&$yymm<=$es2){
+     myinc($w,8,$yymm);
+    myinc($w,9,$yymm,$mode);
+    myinc($w,10,$yymm,$dxcc);
+    myinc($w,11,$yymm,$callsign);
+    myinc($w,12,$yymm,$wpx);
+    if($row["lotw"]==1)myinc($w,13,$yymm);
+    if($row["eqsl"]==1)myinc($w,14,$yymm);
+    if($row["qrz"]==1)myinc($w,15,$yymm);
+  }
+  
 }
 mysqli_free_result($query);
 
-printf("<p id=\"myh1\">%4s %7s %7s %7s %7s %8s %7s %4s %8s %8s %8s</p>","Year","QSO","QSO.cw","QSO.dg","QSO.ph","QSO.uniq","QSO.wpx","DXCC","QSL.LOTW","QSL.EQSL","QSL.QRZ");
+printf("<p id=\"myh1\">%10s %7s %7s %7s %7s %8s %7s %4s %8s %8s %8s</p>","YYYY-MM","QSO","QSO.cw","QSO.dg","QSO.ph","QSO.uniq","QSO.wpx","DXCC","QSL.LOTW","QSL.EQSL","QSL.QRZ");
+$key=array_keys($w[8]);
+rsort($key);
+foreach($key as &$kk)@printf("%10s %7d %7d %7d %7d %8d %7d %4d %8d %8d %8d\n",$kk,$w[08][$kk],$w[9][$kk]["CW"],$w[9][$kk]["DG"],$w[9][$kk]["PH"],count($w[11][$kk]),count($w[12][$kk]),count($w[10][$kk]),$w[13][$kk],$w[14][$kk],$w[15][$kk]);
+echo "\n";
+
+printf("<p id=\"myh1\">%10s %7s %7s %7s %7s %8s %7s %4s %8s %8s %8s</p>","YYYY","QSO","QSO.cw","QSO.dg","QSO.ph","QSO.uniq","QSO.wpx","DXCC","QSL.LOTW","QSL.EQSL","QSL.QRZ");
 $key=array_keys($w[0]);
 rsort($key);
-foreach($key as &$kk)@printf("%4s %7d %7d %7d %7d %8d %7d %4d %8d %8d %8d\n",$kk,$w[0][$kk],$w[1][$kk]["CW"],$w[1][$kk]["DG"],$w[1][$kk]["PH"],count($w[3][$kk]),count($w[4][$kk]),count($w[2][$kk]),$w[5][$kk],$w[6][$kk],$w[7][$kk]);
+foreach($key as &$kk)@printf("%10s %7d %7d %7d %7d %8d %7d %4d %8d %8d %8d\n",$kk,$w[0][$kk],$w[1][$kk]["CW"],$w[1][$kk]["DG"],$w[1][$kk]["PH"],count($w[3][$kk]),count($w[4][$kk]),count($w[2][$kk]),$w[5][$kk],$w[6][$kk],$w[7][$kk]);
 echo "\n";
 
 
