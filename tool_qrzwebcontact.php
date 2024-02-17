258db2491beeda4ef0e2949d6a840c9cf28b05fe
Gianluca Mazzini <gianluca@mazzini.org>
2024-02-18 00:01:49 +0100

    Update tool_qrzwebcontact.php

--- tool_qrzwebcontact.php
+++ tool_qrzwebcontact.php
@@ -4,8 +4,7 @@ $con=mysqli_connect("127.0.0.1",$dbuser,$dbpassword,$dbname);
 mysqli_query($con,"SET time_zone='+00:00'");
 
 $mycall="IK4LZH";
-$mystart="2024-01-01 00:00:00";
-$query=mysqli_query($con,"select distinct callsign from log where mycall='$mycall' and callsign not in (select callsign from qrzwebcontact where sent=1) order by callsign");
+$query=mysqli_query($con,"select distinct callsign from log where mycall='$mycall' and callsign not in (select callsign from qrzwebcontact where mycall='$mycall' and sent=1) order by callsign");
 for(;;){
   $row=mysqli_fetch_assoc($query);
   if($row==null)break;
