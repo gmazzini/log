714e50fc5bdb03fd8c00ec8ed8b88eb25a6088e8
Gianluca Mazzini <gianluca@mazzini.org>
2024-02-18 07:35:31 +0100

    Update tool_qrzwebcontact.php

--- tool_qrzwebcontact.php
+++ tool_qrzwebcontact.php
@@ -18,6 +18,11 @@ for(;;){
 }
 mysqli_free_result($query);
 
+$out=myqrzwebcontact($mycall);
+foreach($out as $v){
+  mysqli_query($con,"insert ignore into qrzwebcontact (mycall,callsign,sent,source) value ('$mycall','$v',1,'web')");
+}
+
 $query=mysqli_query($con,"select callsign from qrzwebcontact where mycall='$mycall' and sent=0 and source='me' order by rand() limit $process");
 for(;;){
   $row=mysqli_fetch_assoc($query);
@@ -37,5 +42,41 @@ for(;;){
 mysqli_free_result($query);
 
 mysqli_close($con);
+
+function myqrzwebcontact($call){
+  $agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36";
+  $ch=curl_init();
+  curl_setopt($ch,CURLOPT_URL,"https://www.qrz.com/lookup/$call");
+  curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+  curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
+  curl_setopt($ch,CURLOPT_HTTPHEADER,Array("User-Agent: $agent"));
+  $out=curl_exec($ch);
+  curl_close($ch);
+  $tok='var wc_summary = "';
+  $l1=strpos($out,$tok,0);
+  if($l1===false)return null;
+  $l1+=strlen($tok);
+  $l2=strpos($out,'"',$l1);
+  $myurl=substr($out,$l1,$l2-$l1);
+
+  $ch=curl_init();
+  curl_setopt($ch,CURLOPT_URL,$myurl);
+  curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+  curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
+  curl_setopt($ch,CURLOPT_HTTPHEADER,Array("User-Agent: $agent"));
+  $out=curl_exec($ch);
+  curl_close($ch);
+  $tok='href="https://www.qrz.com/db/';
+  $l2=0;
+  $tot=0;
+  for(;;){
+    $l1=strpos($out,$tok,$l2);
+    if($l1===false)break;
+    $l1+=strlen($tok);
+    $l2=strpos($out,'/',$l1);
+    if($l2-$l1>0)$dd[$tot++]=substr($out,$l1,$l2-$l1);
+  }
+  return $dd;
+}
 
 ?>
