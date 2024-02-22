072b8c71069d9ff25f2721bf1cf0b4c5fe4533c6
Gianluca Mazzini <gianluca@mazzini.org>
2024-02-22 19:19:32 +0100

    Update tool_qrzwc_my.php

--- tool_qrzwc_my.php
+++ tool_qrzwc_my.php
@@ -18,7 +18,7 @@ for(;;){
 }
 mysqli_free_result($query);
 
-$query=mysqli_query($con,"select distinct callsign from log where mycall='$mycall' and callsign in (select callsign from qrzwebcontact where mycall='$mycall' and source='oth') order by callsign");
+$query=mysqli_query($con,"select distinct callsign from log where mycall='$mycall' and callsign in (select callsign from qrzwebcontact where mycall='$mycall' and source!='me') order by callsign");
 for(;;){
   $row=mysqli_fetch_assoc($query);
   if($row==null)break;
@@ -34,7 +34,7 @@ foreach($out as $v){
   $aux=(int)$row1[0];
   mysqli_free_result($query1);
   if($aux==0)mysqli_query($con,"insert into qrzwebcontact (mycall,callsign,sent,source) value ('$mycall','$v',1,'web')");
-  else mysqli_query($con,"update qrzwebcontact set sent=1,source='web' where mycall='$mycall' and callsign='$v'");
+  else mysqli_query($con,"update qrzwebcontact set sent=1 where mycall='$mycall' and callsign='$v'");
 }
 
 mysqli_close($con);
