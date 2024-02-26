976db2dd619220c4c75fbc75d0f3d82e94b4ad29
Gianluca Mazzini <gianluca@mazzini.org>
2024-02-26 06:58:50 +0100

    Update and rename tool_qrzwc_FINIRE.php to tool_qrzwc_setme.php

--- tool_qrzwc_setme.php
+++ tool_qrzwc_setme.php
@@ -1,12 +1,30 @@
 <?php
+include "local.php";
+include "utility.php";
+include "def_qrz.php";
+include "def_qrzwc.php";
+$mycall="IK4LZH";
+$myshow=0;
+$process=1;
 
-$co=json_decode(file_get_contents("c1.txt"),true);
-$call="2E0GSZ";
+$con=mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
+mysqli_query($con,"SET time_zone='+00:00'");
+$co=json_decode(file_get_contents("/home/www/qrz_cookie"),true);
+
+$query=mysqli_query($con,"select callsign from qrzwebcontact where mycall='$mycall' and looked=1 and me=0 and you=1 and Nwc>10 order by rand()");
+$myprocess=0;
+for(;;){
+  $myprocess++;
+  if($myprocess>$process)break;
+  $row=mysqli_fetch_assoc($query);
+  if($row==null)break;
+  $callsign=$row["callsign"];
+  echo "$myprocess Setup $callsign\n";
 
   $dd=array();
   $agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36";
   $ch=curl_init();
-  curl_setopt($ch,CURLOPT_URL,"https://www.qrz.com/lookup/$call");
+  curl_setopt($ch,CURLOPT_URL,"https://www.qrz.com/lookup/$callsign");
   curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
   curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
   curl_setopt($ch,CURLOPT_HTTPHEADER,Array("User-Agent: $agent"));
@@ -18,7 +36,9 @@ $call="2E0GSZ";
   $l1+=strlen($tok);
   $l2=strpos($out,'"',$l1);
   $myurl=substr($out,$l1,$l2-$l1);
-
+  if(strlen($myurl)<5)continue;
+  echo "... myurl $myusr\n";
+  
   $cookie="";
   foreach($co as $v)$cookie.=$v["name"]."=".$v["value"]."; ";
   $ch=curl_init();
@@ -36,10 +56,12 @@ file_put_contents("c2.txt",$out);
   $l1+=strlen($tok);
   $l2=strpos($out,'"',$l1);
   $userid=substr($out,$l1,$l2-$l1);
+  echo "... userid $userid\n";
+  continue;
 
   $params="webcon=1&wc_userid=$userid";
   $ch=curl_init();
-  curl_setopt($ch,CURLOPT_URL,"https://www.qrz.com/db/".$call);
+  curl_setopt($ch,CURLOPT_URL,"https://www.qrz.com/db/".$callsign);
   curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
   curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
   curl_setopt($ch,CURLOPT_COOKIE,$cookie);
@@ -48,5 +70,8 @@ file_put_contents("c2.txt",$out);
   curl_setopt($ch,CURLOPT_HTTPHEADER,Array("User-Agent: $agent"));
   curl_exec($ch);
   curl_close($ch);
+}
+mysqli_free_result($query);
+mysqli_close($con);
 
 ?>
