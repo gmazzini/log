b952ef841ac8a03232e788088458dedeb20f5fbd
Gianluca Mazzini <gianluca@mazzini.org>
2024-02-26 08:09:35 +0100

    Update tool_qrzwc_setme.php

--- tool_qrzwc_setme.php
+++ tool_qrzwc_setme.php
@@ -14,7 +14,7 @@ $co=json_decode(file_get_contents("/home/www/data/qrz_cookie"),true);
 $query=mysqli_query($con,"select callsign from qrzwebcontact where mycall='$mycall' and looked>0 and me=0 and you=1 and Nwc>10 order by rand()");
 $myprocess=0;
 for(;;){
-  sleep(rand(20,30));
+  sleep(rand(10,20));
   $myprocess++;
   if($myprocess>$process)break;
   $row=mysqli_fetch_assoc($query);
