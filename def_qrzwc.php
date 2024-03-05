98c388fea79e1fe7f0eda50f7a6a07f06acf6174
Gianluca Mazzini <gianluca@mazzini.org>
2024-03-06 03:21:47 +0400

    Update def_qrzwc.php

--- def_qrzwc.php
+++ def_qrzwc.php
@@ -1,5 +1,58 @@
 <?php
 
+function myqrzsetwebcontact($callsign){
+  $agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36";
+  $ch=curl_init();
+  curl_setopt($ch,CURLOPT_URL,"https://www.qrz.com/lookup/$callsign");
+  curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+  curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
+  curl_setopt($ch,CURLOPT_HTTPHEADER,Array("User-Agent: $agent"));
+  $out=curl_exec($ch);
+  curl_close($ch);
+  $tok='var wc_summary = "';
+  $l1=strpos($out,$tok,0);
+  if($l1===false)return 0;
+  $l1+=strlen($tok);
+  $l2=strpos($out,'"',$l1);
+  $myurl=substr($out,$l1,$l2-$l1);
+  if(strlen($myurl)<5)return 0;
+  echo "... myurl $myurl\n";
+
+  $co=json_decode(file_get_contents("/home/www/data/qrz_cookie"),true);
+  $cookie="";
+  foreach($co as $v)$cookie.=$v["name"]."=".$v["value"]."; ";
+  $ch=curl_init();
+  curl_setopt($ch,CURLOPT_URL,$myurl);
+  curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+  curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
+  curl_setopt($ch,CURLOPT_COOKIE,$cookie);
+  curl_setopt($ch,CURLOPT_HTTPHEADER,Array("User-Agent: $agent"));
+  $out=curl_exec($ch);
+  curl_close($ch);
+  $tok='name="wc_userid" value="';
+  $l1=strpos($out,$tok,0);
+  if($l1===false)return 0;
+  $l1+=strlen($tok);
+  $l2=strpos($out,'"',$l1);
+  $userid=(int)substr($out,$l1,$l2-$l1);
+  if($userid==0)return 0;
+  echo "... userid $userid\n";
+
+  $params="webcon=1&wc_userid=$userid";
+  $ch=curl_init();
+  curl_setopt($ch,CURLOPT_URL,"https://www.qrz.com/db/".$callsign);
+  curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+  curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
+  curl_setopt($ch,CURLOPT_COOKIE,$cookie);
+  curl_setopt($ch,CURLOPT_POST,1);
+  curl_setopt($ch,CURLOPT_POSTFIELDS,$params);
+  curl_setopt($ch,CURLOPT_HTTPHEADER,Array("User-Agent: $agent"));
+  curl_exec($ch);
+  curl_close($ch);
+
+  return 1;
+}
+
 function myqrzwebcontact($call,&$Ewc){
   $dd=array();
   $Ewc=0;
