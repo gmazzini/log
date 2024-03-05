60a661a2d0acfbbcd8e1cf4ad73acaf8234dbb65
Gianluca Mazzini <gianluca@mazzini.org>
2024-03-06 03:23:44 +0400

    Update tool_qrzwc_setme.php

--- tool_qrzwc_setme.php
+++ tool_qrzwc_setme.php
@@ -5,11 +5,10 @@ include "def_qrz.php";
 include "def_qrzwc.php";
 $mycall="IK4LZH";
 $myshow=0;
-$process=1000;
+$process=1;
 
 $con=mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
 mysqli_query($con,"SET time_zone='+00:00'");
-$co=json_decode(file_get_contents("/home/www/data/qrz_cookie"),true);
 
 $query=mysqli_query($con,"select callsign from qrzwebcontact where mycall='$mycall' and looked>0 and me=0 and you=1 and Ewc=1 order by Nwc desc");
 $myprocess=0;
@@ -22,54 +21,7 @@ for(;;){
   $callsign=$row["callsign"];
   echo "$myprocess Setup $callsign\n";
 
-  $dd=array();
-  $agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36";
-  $ch=curl_init();
-  curl_setopt($ch,CURLOPT_URL,"https://www.qrz.com/lookup/$callsign");
-  curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-  curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
-  curl_setopt($ch,CURLOPT_HTTPHEADER,Array("User-Agent: $agent"));
-  $out=curl_exec($ch);
-  curl_close($ch);
-  $tok='var wc_summary = "';
-  $l1=strpos($out,$tok,0);
-  if($l1===false)return null;
-  $l1+=strlen($tok);
-  $l2=strpos($out,'"',$l1);
-  $myurl=substr($out,$l1,$l2-$l1);
-  if(strlen($myurl)<5)continue;
-  echo "... myurl $myurl\n";
-  
-  $cookie="";
-  foreach($co as $v)$cookie.=$v["name"]."=".$v["value"]."; ";
-  $ch=curl_init();
-  curl_setopt($ch,CURLOPT_URL,$myurl);
-  curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-  curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
-  curl_setopt($ch,CURLOPT_COOKIE,$cookie);
-  curl_setopt($ch,CURLOPT_HTTPHEADER,Array("User-Agent: $agent"));
-  $out=curl_exec($ch);
-  curl_close($ch);
-  $tok='name="wc_userid" value="';
-  $l1=strpos($out,$tok,0);
-  if($l1===false)return null;
-  $l1+=strlen($tok);
-  $l2=strpos($out,'"',$l1);
-  $userid=(int)substr($out,$l1,$l2-$l1);
-  if($userid==0)continue;
-  echo "... userid $userid\n";
-
-  $params="webcon=1&wc_userid=$userid";
-  $ch=curl_init();
-  curl_setopt($ch,CURLOPT_URL,"https://www.qrz.com/db/".$callsign);
-  curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-  curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
-  curl_setopt($ch,CURLOPT_COOKIE,$cookie);
-  curl_setopt($ch,CURLOPT_POST,1);
-  curl_setopt($ch,CURLOPT_POSTFIELDS,$params);
-  curl_setopt($ch,CURLOPT_HTTPHEADER,Array("User-Agent: $agent"));
-  curl_exec($ch);
-  curl_close($ch);
+  if(!myqrzsetwebcontact($callsign))continue;
   mysqli_query($con,"update qrzwebcontact set me=1 where mycall='$mycall' and callsign='$callsign'");
   
 }
