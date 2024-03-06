f5798f3ca6cd531b74f1a355a8869926e9a12b37
Gianluca Mazzini <gianluca@mazzini.org>
2024-03-06 22:19:12 +0400

    Update tool_qrzwc_my.php

--- tool_qrzwc_my.php
+++ tool_qrzwc_my.php
@@ -29,7 +29,7 @@ for(;;){
 }
 mysqli_free_result($query);
 
-$out=myqrzwebcontact($mycall,$Ewc);
+$out=myqrzwebcontact($mycall,$Ewc,$visit);
 foreach($out as $v){
   $query1=mysqli_query($con,"select count(*) from qrzwebcontact where mycall='$mycall' and callsign='$v'");
   $row1=mysqli_fetch_row($query1);
