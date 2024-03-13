1c4337df5ed1f4b38b72b8e6951880c23f02c8f5
Gianluca Mazzini <gianluca@mazzini.org>
2024-03-13 05:14:08 +0100

    Create tool_qrzwc_who.php

---
+++ tool_qrzwc_who.php
@@ -0,0 +1,30 @@
+<?php
+include "local.php";
+include "utility.php";
+include "def_qrz.php";
+include "def_qrzwc.php";
+$mycall="IK4LZH";
+$myshow=0;
+$process=10000;
+
+$con=mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
+mysqli_query($con,"SET time_zone='+00:00'");
+
+$i=0;
+$query=mysqli_query($con,"select callsign from qrzwebcontact where mycall='$mycall' and sent=1 and qrzed=0 order by rand()");
+for(;;){
+  $row=mysqli_fetch_assoc($query);
+  if($row==null)break;
+  $callsign=$row["callsign"];
+
+  qrz($con,$callsign);
+  echo "$i $callsign\n";
+  mysqli_query($con,"update qrzwebcontact set qrzed=1 where mycall='$mycall' and callsign='$callsign'");
+  sleep(1);
+  $i++;
+  if($i==$process)break;
+}
+mysqli_free_result($query);
+mysqli_close($con);
+
+?>
