06aa1572136557f5a9cdcea8e595badfc2235add
Gianluca Mazzini <gianluca@mazzini.org>
2024-05-10 07:38:02 +0200

    Create tool_analysis.php

---
+++ tool_analysis.php
@@ -0,0 +1,22 @@
+<?php
+include "local.php";
+$con=mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
+mysqli_query($con,"SET time_zone='+00:00'");
+
+$mycall="IK4LZH";
+
+$query=mysqli_query($con,"select signalrx,signaltx from log where mycall='$mycall' and mode='FT8'");
+for(;;){
+  $row=mysqli_fetch_assoc($query);
+  if($row==null)break;
+  $signaltx=$row["signaltx"];
+  $signalrx=$row["signalrx"];
+  $diff=$signaltx-$signalrx;
+  @ $cc[$diff]++;
+}
+mysqli_free_result($query);
+
+print_r($diff);
+
+mysqli_close($con);
+?>
