4d5ee03b77a9a5a8aee322db0bbbbce45a4d9ce3
Gianluca Mazzini <gianluca@mazzini.org>
2024-05-10 09:28:27 +0200

    Update tool_analysis.php

--- tool_analysis.php
+++ tool_analysis.php
@@ -5,14 +5,17 @@ mysqli_query($con,"SET time_zone='+00:00'");
 
 $mycall="IK4LZH";
 
-$query=mysqli_query($con,"select signalrx,signaltx from log where mycall='$mycall' and mode='FT8'");
+$query=mysqli_query($con,"select callsign,signalrx,signaltx from log where mycall='$mycall' and mode='FT8'");
 for(;;){
   $row=mysqli_fetch_assoc($query);
   if($row==null)break;
+  $call=$row["callsign"];
   $signaltx=$row["signaltx"];
   $signalrx=$row["signalrx"];
+  $oo=searchcty($con,$call);
   $diff=((int)$signaltx)-((int)$signalrx);
   @$cc[$diff]++;
+  @$cv[$oo["cont"]][$diff]++;
 }
 mysqli_free_result($query);
 ksort($cc);
