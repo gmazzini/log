7585daeee866e09d581bde0d011679ec494b0d6b
Gianluca Mazzini <gianluca@mazzini.org>
2024-05-12 07:15:36 +0200

    Update tool_ts890s.php

--- tool_ts890s.php
+++ tool_ts890s.php
@@ -1,12 +1,20 @@
 <?php
-include "local.php";
+include "login.php";
 $sock1=socket_create(AF_INET,SOCK_DGRAM,0);
-socket_bind($sock1,"0.0.0.0",22222);
+socket_bind($sock1,"0.0.0.0",6780);
 $sock2=socket_create(AF_INET,SOCK_STREAM,0);
 socket_connect($sock2,"10.0.0.10",60000);
+$aux=trim(xx($sock2,"##CN;"));
+if($aux!="##CN1;"){echo "CN problem\n"; exit(-1);}
+$aux=trim(xx($soxk2,"##ID00".sprintf("%d%d%s%s;",strlen($ts890s_login),strlen($ts890s_passwd),$ts890_login,$ts890_passwd)));
+if($aux!="##ID1;"){echo "ID problem\n"; exit(-1);}
 for(;;){
   socket_recvfrom($sock1,$aux,1000,0,$remote_ip,$remote_port);
-  echo $aux."\n";
+  if(strpos($aux,"i")!==false){
+    $aux=xx($sock2,"FA;");
+    $mm=substr($aux,2,-1);
+    socket_write($ss,$mm,strlen($mm));
+  }
 
 }
 
