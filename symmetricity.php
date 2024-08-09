f5b1a956d3971b27bcbfc1848f40d3a10c0358c0
Gianluca Mazzini <gianluca@mazzini.org>
2024-08-09 19:04:27 +0200

    Update symmetricity.php

--- symmetricity.php
+++ symmetricity.php
@@ -1,5 +1,6 @@
 <?php
 include "local.php";
+include "utility.php";
 
 $mycall="IK4LZH";
 // all here is to think
@@ -19,13 +20,13 @@ for(;;){
   if($signaltx<$lowrep || $signaltx>$highrep)continue;
   $signalrx=(int)$row["signalrx"];
   if($signalrx<$lowrep || $signalrx>$highrep)continue;
-  @$acc[$freqMHZ][$signaltx-$signalrx]++;
-  @$tot[$freqMHZ]++;
+  @$acc[$myband[$freqMHZ]][$signaltx-$signalrx]++;
+  @$tot[$myband[$freqMHZ]]++;
 }
 mysqli_free_result($query);
 
-// print_r($acc);
-
+foreach($myband as $ff => $ll)if($ll<=10 && $ll>=160)
+  
 echo "<html>\n";
 echo "<script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>\n";
 echo "<script type='text/javascript'>\n";
@@ -33,9 +34,12 @@ echo "google.charts.load('current',{'packages':['corechart']});\n";
 echo "google.charts.setOnLoadCallback(drawChart);\n";
 echo "function drawChart(){\n";
 echo "var data=google.visualization.arrayToDataTable([\n";
-echo "['Delta','80m','40m','30m','20m','17m','15m','12m','10m'],\n";
-for($i=$lowrep;$i<$highrep;$i++)printf("[%d,%f,%f,%f,%f,%f,%f,%f,%f],\n",$i,$acc[3][$i]/$tot[3],$acc[7][$i]/$tot[7],$acc[10][$i]/$tot[10],$acc[14][$i]/$tot[14],$acc[18][$i]/$tot[18],$acc[21][$i]/$tot[21],$acc[24][$i]/$tot[24],$acc[28][$i]/$tot[28]);
-printf("[%d,%f,%f,%f,%f,%f,%f,%f,%f]\n",$i,$acc[3][$i]/$tot[3],$acc[7][$i]/$tot[7],$acc[10][$i]/$tot[10],$acc[14][$i]/$tot[14],$acc[18][$i]/$tot[18],$acc[21][$i]/$tot[21],$acc[24][$i]/$tot[24],$acc[28][$i]/$tot[28]);
+echo "['Delta'"; foreach($myband as $ff => $ll)if($ll>=10 && $ll<=160)echo ",$ll"; echo "],\n";
+for($i=$lowrep;$i<=$highrep;$i++){
+  echo "[$i"; foreach($myband as $ff => $ll)if($ll>=10 && $ll<=160){echo ","; echo $acc[$ff][$i]/$tot[$ff];} echo "]";
+  if($i<$highrep)echo ",";
+  echo "\n";
+}
 echo "]);\n";
 echo "var options={title:'Channel Symmetricity by IK4LZH',curveType:'function',legend:{position:'bottom'}};\n";
 echo "var chart=new google.visualization.LineChart(document.getElementById('curve_chart'));\n";
