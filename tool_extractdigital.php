7a0f0ad5d37bb7e0c6ffbc76cf555fa6e17447da
Gianluca Mazzini <gianluca@mazzini.org>
2024-08-09 09:25:12 +0200

    Update tool_extractdigital.php

--- tool_extractdigital.php
+++ tool_extractdigital.php
@@ -23,7 +23,28 @@ for(;;){
 }
 mysqli_free_result($query);
 
-print_r($acc);
+// print_r($acc);
+
+echo "<html>\n";
+echo "<script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>\n";
+echo "<script type='text/javascript'>\n";
+echo "google.charts.load('current',{'packages':['corechart']});\n";
+echo "google.charts.setOnLoadCallback(drawChart);\n";
+echo "function drawChart(){\n";
+echo "var data=google.visualization.arrayToDataTable([\n";
+echo "['Year', 'Sales', 'Expenses', 'terzo'],\n";
+echo "['2004',  1000,      400, 1],\n";
+echo "['2005',  1170,      460,2],\n";
+echo "['2006',  660,       1120,3],\n";
+echo "['2007',  1030,      540,4]\n";
+echo "]);\n";
+echo "var options={title:'Company Performance',curveType:'function',legend:{position:'bottom'}};\n";
+echo "var chart=new google.visualization.LineChart(document.getElementById('curve_chart'));\n";
+echo "chart.draw(data,options);\n";
+echo "}\n";
+echo "</script>\n";
+echo "<div id='curve_chart' style='width: 900px; height: 500px'></div>\n";
+echo "</html>\n";
 
 mysqli_close($con);
 ?>
