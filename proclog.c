2daf3f5b5984cee3c51e3392f7123a824bf31136
Gianluca Mazzini <gianluca@mazzini.org>
2025-10-13 05:42:15 +0200

    Update proclog.c

--- proclog.c
+++ proclog.c
@@ -274,8 +274,8 @@ MYSQL_ROW searchcty(MYSQL *con,char *incall){
 }
 
 void incdata(int cha,char *key){
-  int i1,i2;
+  int i1;
   for(i1=0;i1<ndata2[cha];i1++)if(strcmp(data2[cha][i1].lab,key)==0)break;
   if(i1==ndata2[cha]){strcpy(data2[cha][ndata2[cha]].lab,key); data2[cha][ndata2[cha]].num=1; ndata2[cha]++; }
-  else data2[cha][l1].num++;
+  else data2[cha][i1].num++;
 }
