0bf4906bb69cb8d07af8843aca99f746a67b6eca
Gianluca Mazzini <gianluca@mazzini.org>
2025-10-14 06:17:40 +0200

    Update proclog.c

--- proclog.c
+++ proclog.c
@@ -198,7 +198,6 @@ int main(void){
       if(atoi(row[5])==1)incdata2(7,aux1);
     }
     mysql_free_result(res);
-    for(l1=0;l1<ndata2[0];l1++)printf("%d %s\n",data2[0][l1].idx,data2[0][l1].lab);
     qsort(data2[0],ndata2[0],sizeof(struct data2),cmp1);
     qsort(data2[4],ndata2[4],sizeof(struct data2),cmp2);
     for(l1=0;l1<ndata2[0];l1++)printf("%s %ld %ld %ld %ld %ld\n",data2[0][l1].lab,data2[0][l1].num,ndata3[0][data2[0][l1].idx],numdata2(1,data2[0][l1].lab),numdata2(2,data2[0][l1].lab),numdata2(3,data2[0][l1].lab));
