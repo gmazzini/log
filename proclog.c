74657bef02a1373703f7ef42aa6c69b14cb09667
Gianluca Mazzini <gianluca@mazzini.org>
2025-10-14 08:59:44 +0200

    Update proclog.c

--- proclog.c
+++ proclog.c
@@ -208,12 +208,12 @@ int main(void){
     qsort(data2[0],ndata2[0],sizeof(struct data2),cmp1);
     qsort(data2[4],ndata2[4],sizeof(struct data2),cmp2);
     printf("<p id=\"myh1\">%10s %7s %8s %8s %8s %8s %8s</p>","Band/Mode","QSO","QSO.uniq","QSO.wpx","QSL.LOTW","QSL.EQSL","QSL.QRZ");
-    for(c=0;c<8;c++)for(suml[c]=0,l1=0;l1<ndata2[c];l1++)suml[c]+=data2[c][l1].num;
+    for(c=0;c<4;c++)for(suml[c]=0,l1=0;l1<ndata2[c];l1++)suml[c]+=data2[c][l1].num;
     printf("<p id=\"myh2\">%10s %7ld %8ld %8ld %8ld %8ld %8ld</p>","Tot",suml[0],ndata3[0][TOT2-1],ndata3[2][TOT2-1],suml[1],suml[2],suml[3]);
     for(l1=0;l1<ndata2[0];l1++)printf("%10s %7ld %8ld %8ld %8ld %8ld %8ld\n",data2[0][l1].lab,data2[0][l1].num,ndata3[0][data2[0][l1].idx],ndata3[1][data2[0][l1].idx],numdata2(1,data2[0][l1].lab),numdata2(2,data2[0][l1].lab),numdata2(3,data2[0][l1].lab));
     printf("\n");
     printf("<p id=\"myh1\">%10s %7s %8s %8s %8s %8s %8s %s</p>","dxcc","QSO","QSO.uniq","QSO.wpx","QSL.LOTW","QSL.EQSL","QSL.QRZ","Country");
-    printf("<p id=\"myh2\">%10s %7ld %8ld %8ld %8ld %8ld %8ld</p>","Tot",suml[4],ndata3[1][TOT2-1],ndata3[3][TOT2-1],suml[5],suml[6],suml[7]);
+    printf("<p id=\"myh2\">%10s %7ld %8ld %8ld %8ld %8ld %8ld</p>","Tot",ndata2[4],ndata3[1][TOT2-1],ndata3[3][TOT2-1],nadata2[5],ndata2[6],ndata2[7]);
     for(l1=0;l1<ndata2[4];l1++)printf("%10s %7ld %8ld %8ld %8ld %8ld %8ld\n",data2[4][l1].lab,data2[4][l1].num,ndata3[1][data2[4][l1].idx],ndata3[3][data2[4][l1].idx],numdata2(5,data2[04][l1].lab),numdata2(6,data2[4][l1].lab),numdata2(7,data2[4][l1].lab));
     printf("</pre>");
     goto end;
