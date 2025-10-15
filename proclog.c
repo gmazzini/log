49b68efc2c847b786b6bde068a915dd1407c7cf9
Gianluca Mazzini <gianluca@mazzini.org>
2025-10-15 07:18:52 +0200

    Update proclog.c

--- proclog.c
+++ proclog.c
@@ -264,8 +264,8 @@ int main(void){
     mysql_free_result(res);
     for(l1=0;l1<ndata3[0][0];l1++){
       printf("%s %ld %ld %ld %ld",data3[0][0][l1].lab,data3[0][0][l1].num,numdata3(0,4,data3[0][0][l1].lab),numdata3(0,5,data3[0][0][l1].lab),numdata3(0,6,data3[0][0][l1].lab));
-      printf(" %ld %ld %ld",0L,0L,0L);
-      printf("%ld %ld %ld\n",numdata3(0,1,data3[0][0][l1].lab),numdata3(0,2,data3[0][0][l1].lab),numdata3(0,3,data3[0][0][l1].lab));
+      printf(" %ld %ld %ld",ndata3[1][data3[0][0][l1].idx],0L,0L);
+      printf(" %ld %ld %ld\n",numdata3(0,1,data3[0][0][l1].lab),numdata3(0,2,data3[0][0][l1].lab),numdata3(0,3,data3[0][0][l1].lab));
     }
     printf("<pre>");
     goto end;
