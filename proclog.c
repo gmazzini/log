2a1aef2574c26314fdabda6d76a5248efb2bb3f7
Gianluca Mazzini <gianluca@mazzini.org>
2025-10-16 18:47:54 +0200

    Limit output length of rrr to 10 characters

--- proclog.c
+++ proclog.c
@@ -349,7 +349,7 @@ char rrr[10000];
   if(act>=15){
     printf("Status: 200 OK\r\n");
     printf("Content-Type: text/html; charset=utf-8\r\n\r\n");
-    sprintf("%s\n",rrr);
+    sprintf("%.10s\n",rrr);
     printf("</pre>");
     goto end;
   }
