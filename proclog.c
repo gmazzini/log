cbc699a0afa0cbb80ad3288ac9fce9bb0a34d034
Gianluca Mazzini <gianluca@mazzini.org>
2025-10-17 11:21:14 +0200

    Update proclog.c

--- proclog.c
+++ proclog.c
@@ -383,10 +383,10 @@ int main(void){
       epoch=timegm(&ts);
       epoch-=QSLWIN; strftime(aux1,sizeof(aux1),"%Y-%m-%d %H:%M:%S",gmtime(&epoch));
       epoch+=2*QSLWIN; strftime(aux2,sizeof(aux2),"%Y-%m-%d %H:%M:%S",gmtime(&epoch));
-      if(adif1[3][0]=='Y'){
+  //    if(adif1[3][0]=='Y'){
         printf("update log set lotw=1 where mycall='%s' and callsign='%s' and start>='%s' and start<='%s'\n",mycall,adif[0],aux1,aux2);
         // printf("%s\n",buf);
-      }
+   //   }
       gg=adifextract(NULL,adif1,vv);
     }  
     printf("</pre>");
