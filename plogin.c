77f6661c45c55607274f982b6c0822ff25188a8e
Gianluca Mazzini <gianluca@mazzini.org>
2025-10-20 18:57:20 +0200

    Update plogin.c

--- plogin.c
+++ plogin.c
@@ -23,11 +23,8 @@ int main(void){
   if(con==NULL)exit(1);
   if(mysql_real_connect(con,dbhost,dbuser,dbpassword,dbname,0,NULL,0)==NULL)exit(1);
   epoch=time(NULL);
-  printf("1\n");
   sprintf(buf,"select ota from user where mycall='%s' and md5passwd='%s' and lota>%ld limit 1",tok[0],tok[1],epoch);
   mysql_query(con,buf); res=mysql_store_result(con); row=mysql_fetch_row(res);
-    printf("2\n%s\n",buf);
-
   if(row==NULL)exit(1);
   printf("Content-Type: text/plain\r\n\r\n");
   printf("%s",row[0]);
