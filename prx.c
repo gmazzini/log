8e14205972be6d4350a27677f173d69cc84c8105
Gianluca Mazzini <gianluca@mazzini.org>
2025-10-21 20:17:52 +0200

    Update prx.c

--- prx.c
+++ prx.c
@@ -10,30 +10,20 @@ int main(void) {
   int sockfd;
   struct sockaddr_in server_addr,client_addr;
   socklen_t addr_len=sizeof(client_addr);
+  ssize_t len;
   char buffer[1000];
 
   sockfd=socket(AF_INET,SOCK_DGRAM,0);
   if(sockfd<0)return;
   memset(&server_addr,0,sizeof(server_addr));
-    server_addr.sin_family=AF_INET;
-    server_addr.sin_addr.s_addr=INADDR_ANY;
-    server_addr.sin_port=htons(PORT);
-
-    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("bind");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-    for (;;) {
-        ssize_t len = recvfrom(sockfd, buffer, sizeof(buffer) - 1, 0,
-                               (struct sockaddr *)&client_addr, &addr_len);
-        if (len < 0) {
-            perror("recvfrom");
-            continue;
-        }
-
-        buffer[len] = '\0';
+  server_addr.sin_family=AF_INET;
+  server_addr.sin_addr.s_addr=INADDR_ANY;
+  server_addr.sin_port=htons(PORT);
+  if(bind(sockfd,(struct sockaddr *)&server_addr,sizeof(server_addr))<0)return;
+  for(;;){
+    len=recvfrom(sockfd,buffer,sizeof(buffer)-1,0,(struct sockaddr *)&client_addr,&addr_len);
+    if(len<0)continue;
+    buffer[len]='\0';
         printf("%s\n", buffer);
         fflush(stdout);
     }
