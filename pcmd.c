5667be2c344680b30f08e3c0d645dc63e404bbd6
Gianluca Mazzini <gianluca@mazzini.org>
2025-10-22 19:06:26 +0100

    Update pcmd.c

--- pcmd.c
+++ pcmd.c
@@ -7,7 +7,11 @@
 
 int main(void) {
   int c,vv,gg;
-  char buf[1000],aux1[300],tok[4][100];
+  char buf[1000],aux1[300],tok[4][100],mycall[16],*p;
+  MYSQL *con;
+  MYSQL_RES *res;
+  MYSQL_ROW row,row1;
+  time_t epoch;
 
   for(vv=0,gg=0;;){
     c=getchar();
@@ -16,10 +20,24 @@ int main(void) {
     if(vv<4)tok[vv][gg++]=(char)c;
   }
   tok[vv][gg]='\0';
+  con=mysql_init(NULL);
+  if(con==NULL)exit(1);
+  if(mysql_real_connect(con,dbhost,dbuser,dbpassword,dbname,0,NULL,0)==NULL)exit(1);
+  mysql_query(con,"SET time_zone='+00:00'");
+  epoch=time(NULL);
+  sprintf(buf,"select mycall from user where ota='%s' and lota>%ld limit 1",tok[0],epoch);
+  mysql_query(con,buf); res=mysql_store_result(con); row=mysql_fetch_row(res);
+  if(row==NULL)exit(1);
+  strcpy(mycall,row[0]);
+  mysql_free_result(res);
 
+  p=strtok(tok[4],":");
+  if(strcmp(p,"DEL")==0 || strcmp(p,"DELETE")==0)sprintf(buf,"delete from log where mycall='%s' and callsign='%s' and start='%s'",mycall,tok[1],tok[2]);
+  
+  
 FILE *fp;
   fp=fopen("/home/www/log/pcmd.txt","w");
-  fprintf(fp,"%s %s %s %s\n",tok[0],tok[1],tok[2],tok[3]);
+  fprintf(fp,"%s,%s,%s,%s,%s\n",tok[0],tok[1],tok[2],tok[3],buf);
   fclose(fp);
   
 }
