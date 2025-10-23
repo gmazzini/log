312b4e3c5a8ae1ce5deaccf3ec70f524077d4689
Gianluca Mazzini <gianluca@mazzini.org>
2025-10-23 08:13:48 +0100

    Update proclog.c

--- proclog.c
+++ proclog.c
@@ -730,11 +730,14 @@ char *search(char *buf,char *key){
   return out;
 }
 
-void qrz(){
+void qrz(char *call){
   struct addrinfo h={0},*r=0;
   int s,n;
-  char buf[100001],aux1[300];
-  const char *qrzkey[]={"fname","nickname","name","addr1","addr2","state","zip","country","grid","email","cqzone","ituzone","born","image"};
+  char buf[100001],aux1[300],key[13][201],ee[40];
+  time_t now;
+  struct tm *utc;
+  
+  const char *qrzkey[13]={"fname","name","addr1","addr2","state","zip","country","grid","email","cqzone","ituzone","born","image"};
   h.ai_socktype=SOCK_STREAM;
   getaddrinfo("xmldata.qrz.com","80",&h,&r);
   s=socket(r->ai_family,r->ai_socktype,r->ai_protocol);
@@ -747,13 +750,14 @@ void qrz(){
   close(s);
   s=socket(r->ai_family,r->ai_socktype,r->ai_protocol);
   connect(s,r->ai_addr,r->ai_addrlen);
-  sprintf(buf,"GET /xml/current/?s=%s;callsign=%s HTTP/1.0\r\nHost: xmldata.qrz.com\r\nConnection: close\r\n\r\n",aux1,argv[3]);
+  sprintf(buf,"GET /xml/current/?s=%s;callsign=%s HTTP/1.0\r\nHost: xmldata.qrz.com\r\nConnection: close\r\n\r\n",aux1,call);
   send(s,buf,strlen(buf),0);
   n=recv(s,buf,10000,0);
   buf[n]='\0';
-  s=sizeof(qrzkey)/sizeof(qrzkey[0]);
-  for(n=0;n<s;n++)strcpy(aux1,search(buf,(char *)qrzkey[n]));
-    
+  for(n=0;n<13;n++)strcpy(key[n],search(buf,(char *)qrzkey[n]),200);
+  now=time(NULL); utc=gmtime(&now); strftime(ee,39,"%Y-%m-%d %H:%M:%S",utc);
+  sprintf(buf,"replace into who (callsign,firstname,lastname,addr1,addr2,state,zip,country,grid,email,cqzone,ituzone,born,image,myupdate,src) value ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s',%d,%d,%d,'%s','%s','qrz.com')",call,key[0],key[1],key[2],key[3],key[4],key[5],key[6],key[7],key[8],atoi(key[9]),atoi(key[10]),atoi(key[11]),key[12],ee)
+        
   
   close(s);
 }
