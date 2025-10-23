a952afc3ffb9a3cdceb670e698995ac945978eb4
Gianluca Mazzini <gianluca@mazzini.org>
2025-10-23 12:44:34 +0100

    Update proclog.c

--- proclog.c
+++ proclog.c
@@ -4,6 +4,10 @@
 #include <string.h>
 #include <time.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 #include <mysql/mysql.h>
 #include "log.def"
 #define TOT3 5
