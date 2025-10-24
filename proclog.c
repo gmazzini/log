12c8de32c57320b7dc2f1f630f5f86ae4703aaf4
Gianluca Mazzini <gianluca@mazzini.org>
2025-10-24 17:16:18 +0100

    Update proclog.c

--- proclog.c
+++ proclog.c
@@ -781,6 +781,49 @@ size_t write_cb(void *ptr,size_t size,size_t nmemb,void *userdata){
   return size*nmemb;
 }
 
+char *cyrlat(char *input){
+  static char output[800],*p;
+  const char *cyr[]={
+    "а","б","в","г","д","е","ё","ж","з","и","й","к","л","м","н","о","п","р","с","т","у",
+    "ф","х","ц","ч","ш","щ","ъ","ы","ь","э","ю","я",
+    "А","Б","В","Г","Д","Е","Ё","Ж","З","И","Й","К","Л","М","Н","О","П","Р","С","Т","У",
+    "Ф","Х","Ц","Ч","Ш","Щ","Ъ","Ы","Ь","Э","Ю","Я"
+  };
+  const char *lat[]={
+    "a","b","v","g","d","e","e","zh","z","i","y","k","l","m","n","o","p","r","s","t","u",
+    "f","h","ts","ch","sh","sht","i","y","y","e","yu","ya",
+    "A","B","V","G","D","E","E","Zh","Z","I","Y","K","L","M","N","O","P","R","S","T","U",
+    "F","H","Ts","Ch","Sh","Sht","I","Y","Y","E","Yu","Ya"
+  };
+  int matched;
+  size_t i,clen,len;
+  
+  size_t n=sizeof(cyr)/sizeof(cyr[0]);
+  output[0]='\0';
+  p=input;
+  while(*p && strlen(output)<sizeof(output)-1){
+    matched=0;
+    for(i=0;i<n;i++){
+      clen=strlen(cyr[i]);
+      if(strncmp(p,cyr[i],clen)==0){
+        strncat(output,lat[i],sizeof(output)-strlen(output)-1);
+        p+=clen;
+        matched=1;
+        break;
+      }
+    }
+    if(!matched){
+      len=strlen(output);
+      if(len<sizeof(output)-1){
+        output[len]=*p;
+        output[len+1]='\0';
+      }
+      p++;
+    }
+  }
+  return output;
+}
+
 void qrzru(MYSQL *con,char *call){
   CURL *h;
   char aux1[300],aux2[300],key[12][201];
@@ -813,7 +856,8 @@ void qrzru(MYSQL *con,char *call){
   curl_easy_cleanup(h);
   for(n=0;n<12;n++)strcpy(key[n],search(wrbuf,(char *)qrzkey[n]));
   printf("<pre>");
-  for(n=0;n<12;n++)printf("%s: %s\n",qrzkey[n],key[n]);
+  for(n=0;n<12;n++)printf("%s: %s\n",qrzkey[n],cyrlat(key[n]));
   printf("</pre>");
   printf("Risposta:\n%s\n",wrbuf);
 }
+
