38496720e5043d6f6720f13193fd9580a36a2347
Gianluca Mazzini <gianluca@mazzini.org>
2025-10-30 06:34:33 +0100

    Update pcmd.c

--- pcmd.c
+++ pcmd.c
@@ -34,48 +34,48 @@ int main(void) {
   strcpy(mycall,row[0]);
   mysql_free_result(res);
 
-  p=strtok(tok[3],":");
+  p=strtok(tok[3],"|");
   buf[0]='\0';
   if(strcmp(p,"DEL")==0 || strcmp(p,"DELETE")==0)
     sprintf(buf,"delete from log where mycall='%s' and callsign='%s' and start='%s'",mycall,tok[2],tok[1]);
   else if(strcmp(p,"FT")==0 || strcmp(p,"FREQTX")==0){
-    p=strtok(NULL,":");
+    p=strtok(NULL,"|");
     if(p!=NULL)sprintf(buf,"update log set freqtx=%ld where mycall='%s' and callsign='%s' and start='%s'",atol(p)*1000L,mycall,tok[2],tok[1]);
   }
   else if(strcmp(p,"FR")==0 || strcmp(p,"FREQRX")==0){
-    p=strtok(NULL,":");
+    p=strtok(NULL,"|");
     if(p!=NULL)sprintf(buf,"update log set freqrx=%ld where mycall='%s' and callsign='%s' and start='%s'",atol(p)*1000L,mycall,tok[2],tok[1]);
   }
   else if(strcmp(p,"M")==0 || strcmp(p,"MODE")==0){
-    p=strtok(NULL,":");
+    p=strtok(NULL,"|");
     if(p!=NULL)sprintf(buf,"update log set mode='%s' where mycall='%s' and callsign='%s' and start='%s'",p,mycall,tok[2],tok[1]);
   }
   else if(strcmp(p,"ST")==0 || strcmp(p,"SIGNALTX")==0){
-    p=strtok(NULL,":");
+    p=strtok(NULL,"|");
     if(p!=NULL)sprintf(buf,"update log set signaltx='%s' where mycall='%s' and callsign='%s' and start='%s'",p,mycall,tok[2],tok[1]);
   }
   else if(strcmp(p,"SR")==0 || strcmp(p,"SIGNALRX")==0){
-    p=strtok(NULL,":");
+    p=strtok(NULL,"|");
     if(p!=NULL)sprintf(buf,"update log set signalrx='%s' where mycall='%s' and callsign='%s' and start='%s'",p,mycall,tok[2],tok[1]);
   }
   else if(strcmp(p,"C")==0 || strcmp(p,"CALL")==0){
-    p=strtok(NULL,":");
+    p=strtok(NULL,"|");
     if(p!=NULL)sprintf(buf,"update log set callsign='%s' where mycall='%s' and callsign='%s' and start='%s'",p,mycall,tok[2],tok[1]);
   }
   else if(strcmp(p,"DT")==0 || strcmp(p,"DATETIME")==0){
-    p=strtok(NULL,":");
+    p=strtok(NULL,"|");
     if(p!=NULL)sprintf(buf,"update log set start='%s' where mycall='%s' and callsign='%s' and start='%s'",p,mycall,tok[2],tok[1]);
   }
   else if(strcmp(p,"CO")==0 || strcmp(p,"CONTEST")==0){
-    p=strtok(NULL,":");
+    p=strtok(NULL,"|");
     if(p!=NULL)sprintf(buf,"update log set contest='%s' where mycall='%s' and callsign='%s' and start='%s'",p,mycall,tok[2],tok[1]);
   }
   else if(strcmp(p,"COT")==0 || strcmp(p,"CONTESTTX")==0){
-    p=strtok(NULL,":");
+    p=strtok(NULL,"|");
     if(p!=NULL)sprintf(buf,"update log set contesttx='%s' where mycall='%s' and callsign='%s' and start='%s'",p,mycall,tok[2],tok[1]);
   }
   else if(strcmp(p,"COR")==0 || strcmp(p,"CONTESTRX")==0){
-    p=strtok(NULL,":");
+    p=strtok(NULL,"|");
     if(p!=NULL)sprintf(buf,"update log set contestrx='%s' where mycall='%s' and callsign='%s' and start='%s'",p,mycall,tok[2],tok[1]);
   }
   if(buf[0]!='\0')mysql_query(con,buf);
