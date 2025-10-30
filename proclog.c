64a3c4958c254e2fe0f7fd5ca9afa60c9309ea0b
Gianluca Mazzini <gianluca@mazzini.org>
2025-10-30 16:45:17 +0100

    Update proclog.c

--- proclog.c
+++ proclog.c
@@ -730,8 +730,8 @@ int main(void){
     for(c=0;c<ndata3[0][2];c++){
       gg=strlen(data3[0][2][c].lab);
       for(l1=0,idx=0;idx<ndata3[0][0];idx++)if(strncmp(data3[0][0][idx].lab,data3[0][2][c].lab,gg)==0)l1+=data3[0][0][idx].num;
-      for(l2=0,idx=0;idx<ndata3[0][0];idx++)if(strncmp(data3[0][1][idx].lab,data3[0][2][c].lab,gg)==0)l1+=data3[0][1][idx].num;
-      printf("%3s %8ld %04ld\n",data3[0][2][c].lab,l1,l2);
+      for(l2=0,idx=0;idx<ndata3[0][1];idx++)if(strncmp(data3[0][1][idx].lab,data3[0][2][c].lab,gg)==0)l1+=data3[0][1][idx].num;
+      printf("%3s %8ld %4ld\n",data3[0][2][c].lab,l1,l2);
     }
     for(l1=0,l2=0;l2<ndata3[0][0];l2++)l1+=data3[0][0][l2].num;
     printf("Points:%ld Cty:%ld Zone:%ld Total:%ld\n",l1,ndata3[0][1],ndata3[0][2],l1*(ndata3[0][1]+ndata3[0][2]));
