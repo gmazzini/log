217f18ff62f1acf3c61d5cb39a24c96b8a96df6e
Gianluca Mazzini <gianluca@mazzini.org>
2025-11-01 06:23:52 +0100

    Update pcluster.c

--- pcluster.c
+++ pcluster.c
@@ -56,7 +56,7 @@ static void *whois_thread(void *arg) {
     show=atol(buf);
     pthread_mutex_lock(&data_mtx);
     for(i=0;i<show;i++){
-      idx=(i-1-show+ELM)%ELM;
+      idx=(ndata-1-show+ELM)%ELM;
       if(data[idx].freq>0){
         tm_now=gmtime(&data[idx].time); te=*tm_now; timegm(&te);
         strftime(aux1,sizeof(aux1),"%Y-%m-%d %H:%M:%S",&te);
