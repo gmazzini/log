1a83a9192d8427463664614c3e8735a37283b197
Gianluca Mazzini <gianluca@mazzini.org>
2025-11-01 16:50:21 +0100

    Update proclog.c

--- proclog.c
+++ proclog.c
@@ -704,7 +704,11 @@ int main(void){
         mysql_query(con,buf); res=mysql_store_result(con); row=mysql_fetch_row(res);
         if(row==NULL)strcpy(aux1,"   "); else strcpy(aux1,myts(atol(row[0])));
         mysql_free_result(res);
-        printf("<button type=\"button\" class=\"myb2\" onclick=\"cmd3('%s','%.1f')\"> </button> %s <b>%16s</b> %8.1f %7d %3s (%s)\n",p4,fx,p1,p4,fx,l1,aux1,p2);
+        sprintf(buf,"select lotw+eqsl+qrz from log where mycall='%s' and callsign='%s'",mycall,p4);
+        mysql_query(con,buf); res=mysql_store_result(con); row=mysql_fetch_row(res);
+        if(row==NULL)l2=0; else l2=atol(row[0]);
+        mysql_free_result(res);
+        printf("<button type=\"button\" class=\"myb2\" onclick=\"cmd3('%s','%.1f')\"> </button> %s <b>%16s</b> %8.1f %7d %7d %3s (%s)\n",p4,fx,p1,p4,fx,l1,l2,aux1,p2);
         pp=qq+1;
       }
     }
