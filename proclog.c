608877edfd2840dd04bb04ef0dc8eec11e38691b
Gianluca Mazzini <gianluca@mazzini.org>
2025-11-02 04:57:49 +0100

    Update proclog.c

--- proclog.c
+++ proclog.c
@@ -705,7 +705,7 @@ int main(void){
         
 
         mysql_query(con,buf); res=mysql_store_result(con); row=mysql_fetch_row(res);
-        if(row==NULL){l3=0; l4=0; strcpy(aux1,"   ");} else{l3=atol(row[0]); l4=atol(row[1]); strcpy(aux1,myts(atol(row[2])));}
+        if(row[2]==NULL){l3=0; l4=0; strcpy(aux1,"   ");} else{l3=atol(row[0]); l4=atol(row[1]); strcpy(aux1,myts(atol(row[2])));}
         mysql_free_result(res);
         printf("<button type=\"button\" class=\"myb2\" onclick=\"cmd3('%s','%.1f')\"> </button> %s <b>%16s</b> %8.1f %7ld %7ld %4ld %4ld %3s (%s)\n",p4,fx,p1,p4,fx,l1,l2,l3,l4,aux1,p2);
         pp=qq+1;
