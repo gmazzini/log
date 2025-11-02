284fbdb779b744605d7016bbb8da3045f7d2f8c2
Gianluca Mazzini <gianluca@mazzini.org>
2025-11-02 16:52:26 +0100

    Update proclog.c

--- proclog.c
+++ proclog.c
@@ -680,7 +680,7 @@ int main(void){
     a.sin_family=AF_INET; a.sin_port=htons(22222);
     inet_pton(AF_INET,"185.119.17.36",&a.sin_addr);
     connect(s,(struct sockaddr*)&a,sizeof(a));
-    sprintf(aux1,"1000\n"); // DA GUARDARE
+    sprintf(aux1,"0,1000\n"); // DA GUARDARE
     send(s,aux1,strlen(aux1),0);
     for(;;){
       l1=recv(s,ff,MAXFF,0);
@@ -691,7 +691,7 @@ int main(void){
         qq=strpbrk(pp,"\n");
         if(!qq)break;
         *qq='\0';
-        p1=strtok(pp,",");
+        p1=strtok(pp,","); 
         p2=strtok(NULL,",");
         p3=strtok(NULL,","); l1=atol(p3); fx=l1/1000.0;
         p4=strtok(NULL,",");
@@ -708,7 +708,9 @@ int main(void){
         mysql_query(con,buf); res=mysql_store_result(con); row=mysql_fetch_row(res);
         if(row[2]==NULL){l3=0; l4=0; strcpy(aux1,"   ");} else{l3=atol(row[0]); l4=atol(row[1]); strcpy(aux1,myts(atol(row[2])));}
         mysql_free_result(res);
-        printf("<button type=\"button\" class=\"myb2\" onclick=\"cmd3('%s','%.1f')\"> </button> %s <b>%16s</b> %10.1f %7ld %7ld %4ld %4ld %3s (%s)\n",p4,fx,p1,p4,fx,l1,l2,l3,l4,aux1,p2);
+        epoch=atol(p1); tm_now=gmtime(&epoch); ts=*tm_now; timegm(&ts); 
+        strftime(aux2,sizeof(aux1),"%Y-%m-%d %H:%M:%S",&ts);
+        printf("<button type=\"button\" class=\"myb2\" onclick=\"cmd3('%s','%.1f')\"> </button> %s <b>%16s</b> %10.1f %7ld %7ld %4ld %4ld %3s (%s)\n",p4,fx,aux2,p4,fx,l1,l2,l3,l4,aux1,p2);
       }
     }
     close(s);
