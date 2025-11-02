a29a5d75129810a56fa896993e156abd6879a258
Gianluca Mazzini <gianluca@mazzini.org>
2025-11-02 05:33:23 +0100

    Update proclog.c

--- proclog.c
+++ proclog.c
@@ -698,10 +698,11 @@ int main(void){
         p4=strtok(NULL,",");
         row=searchcty(con,p4); vv=atoi(row[2]);
         sprintf(buf,"select count(*),sum(lotw+eqsl+qrz) from log where mycall='%s' and dxcc=%d",mycall,vv);
+        printf(">> %s\n",buf);
         mysql_query(con,buf); res=mysql_store_result(con); row=mysql_fetch_row(res); l1=atol(row[0]); l2=atol(row[1]);
         mysql_free_result(res);
         sprintf(buf,"select count(*),sum(lotw)+sum(eqsl)+sum(qrz),timestampdiff(second,max(start),now()) from log where mycall='%s' and callsign='%s'",mycall,p4);
-       printf("%s\n",buf);
+       printf("-- %s\n",buf);
         mysql_query(con,buf); res=mysql_store_result(con); row=mysql_fetch_row(res);
         if(row[2]==NULL){l3=0; l4=0; strcpy(aux1,"   ");} else{l3=atol(row[0]); l4=atol(row[1]); strcpy(aux1,myts(atol(row[2])));}
         mysql_free_result(res);
