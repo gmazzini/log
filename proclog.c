8ad244ad8d3df49385690d1f07e0f1f04c0b75e7
Gianluca Mazzini <gianluca@mazzini.org>
2025-11-04 22:11:20 +0100

    Update proclog.c

--- proclog.c
+++ proclog.c
@@ -134,17 +134,6 @@ int main(void){
     }
     mysql_free_result(res);
     printf("Set dxcc: %ld\nNot found dxcc: %ld\n",l1,l2);
-    sprintf(buf,"select distinct l.callsign from log AS l left join who as w on l.callsign=w.callsign where l.mycall='%s' and (w.email is null or w.callsign is null) order by rand () limit 100",mycall);
-    mysql_query(con,buf);
-    res=mysql_store_result(con);
-    for(;;){
-      row=mysql_fetch_row(res);
-      if(row==NULL)break;
-      qrzcom(con,row[0]);
-      printf("%s\n",row[0]);
-    }
-    mysql_free_result(res);
-
     printf("</pre>");
     goto end;
   }
