20017a8e8d9d5f6ae6cec5b23eaf66f9dbf5b374
Gianluca Mazzini <gianluca@mazzini.org>
2025-11-08 08:30:36 +0100

    Update pscore.c

--- pscore.c
+++ pscore.c
@@ -1,6 +1,6 @@
 // pscore.c contest score function by GM @2025 V 2.0
 
-const char *conid[]={"CQWWSSB","CQWWCW","CQWPXSSB","CQWPXCW","CQWWDIGI","4080","IARUHF","CQ160SSB","CQ160CW","SPDX","LZDX","OKOMSSB","OKOMCW","HADX","ARIDX","KOSSSB","KOSCW","RDAC","ARRLSSB","ARRLCW","RDXC","JIDXSSB","JIDXCW","YODX","CQM","WAESSB","WAECW","WAERTTY","CQ28","UBASSB","UBACW","IOTA","EUHF"};
+const char *conid[]={"CQWWSSB","CQWWCW","CQWPXSSB","CQWPXCW","CQWWDIGI","4080","IARUHF","CQ160SSB","CQ160CW","SPDX","LZDX","OKOMSSB","OKOMCW","HADX","ARIDX","KOSSSB","KOSCW","RDAC","ARRLSSB","ARRLCW","RDXC","JIDXSSB","JIDXCW","YODX","CQM","WAESSB","WAECW","WAERTTY","CQ28","UBASSB","UBACW","IOTA","EUHF","ARISEZ"};
 void conscore(MYSQL *con,char tok[][100],char *mycall){
   int contype,c,gg,vv,cqz[1000],ituz[1000],d,n;
   long l1,l2;
@@ -452,6 +452,19 @@ void conscore(MYSQL *con,char tok[][100],char *mycall){
         incdata3(0,0,aux1,1,0);
         incdata3(0,1,aux1,1,0);
         sprintf(aux2,"%03d:%s",c,row[4]);
+        incdata3(0,2,aux2,1,0); incdata3(0,3,aux2,1,0);
+        sprintf(aux4,"%03d",c);
+        incdata3(0,4,aux4,1,0);
+        break;
+      case 33: // ARISEZ
+        if(gg!=248||vv!=248)break;
+        sprintf(aux1,"%03d:%s:%s",c,row[0],mymode(row[5]));
+        incdata3(0,0,aux1,1,0);
+        if(c==40)incdata3(0,1,aux1,1,0);
+        else if(c==80||c==10)incdata3(0,1,aux1,2,0);
+        else if(c==160||c==15)incdata3(0,1,aux1,3,0);
+        else if(c==10)incdata3(0,1,aux1,4,0);
+        sprintf(aux2,"%03d:%s:%s",c,row[4],mymode(row[5]));
         incdata3(0,2,aux2,1,0); incdata3(0,3,aux2,1,0);
         sprintf(aux4,"%03d",c);
         incdata3(0,4,aux4,1,0);
