29962e7f86d0ee46e2b64a1f2a59c2d5739fd47e
Gianluca Mazzini <gianluca@mazzini.org>
2025-11-11 18:12:30 +0100

    Update proclog.c

--- proclog.c
+++ proclog.c
@@ -608,7 +608,7 @@ int main(void){
       l2=numdata3(0,1,data3[0][0][idx].lab); suml[2]+=l2;
       l3=numdata3(0,2,data3[0][0][idx].lab); suml[3]+=l3;
       l4=numdata3(0,3,data3[0][0][idx].lab); suml[4]+=l4;
-      printf("%6s %8ld %8ld %8ld %8ld\n",data3[0][0][l1].lab,l1,l2,l3,l4);
+      printf("%6s %8ld %8ld %8ld %8ld\n",data3[0][0][idx].lab,l1,l2,l3,l4);
     }
     printf("<p class=\"myh2\">%6s %8ld %8ld %8ld %8ld</p>","ALL",suml[1],suml[2],suml[3],suml[4]);
     printf("</pre>");
