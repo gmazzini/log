943c316fba889b477a427c50cd555a219ce3099d
Gianluca Mazzini <gianluca@mazzini.org>
2025-11-15 14:57:29 +0100

    Update qoth.c

--- qoth.c
+++ qoth.c
@@ -2,7 +2,9 @@
 define MAXWC 100000
 
 int main(){
-  long visit,webcon;
+  long visit,webcon,i;
+  wccall=(char **)malloc(MAXWC*sizeof(char *));
+  for(i=0;i<MAXWC;i++)wccall[i]=(char *)malloc(20*sizeof(char));
   readqrz("ik4lzh",&visit,&webcon);
   printf("%ld\n%ld\n",visit,webcon);
 }
