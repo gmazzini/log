58a95e86103bd89e5e806e3cdb19aa00040c2bf1
Gianluca Mazzini <gianluca@mazzini.org>
2025-11-15 15:23:10 +0100

    Update qfunc.c

--- qfunc.c
+++ qfunc.c
@@ -7,13 +7,19 @@ char **wccall,wrbuf[10000];
 long wcn;
 size_t wrused;
 
-size_t write_cb(void *ptr,size_t size,size_t nmemb,void *userdata){
-  size_t total=size*nmemb;
-  if(wrused+total>=sizeof(wrbuf))total=sizeof(wrbuf)-wrused-1;
-  memcpy(wrbuf+wrused,ptr,total);
-  wrused+=total;
-  wrbuf[wrused]=0;
-  return size*nmemb;
+static size_t write_cb(void *ptr, size_t size, size_t nmemb, void *userdata) {
+    size_t realsize = size * nmemb;
+    char **buffer = (char **)userdata;
+    char *newbuf;
+
+    newbuf = realloc(*buffer, (*buffer ? strlen(*buffer) : 0) + realsize + 1);
+    if (!newbuf) return 0;
+
+    if (!*buffer) newbuf[0] = '\0';
+    strncat(newbuf, ptr, realsize);
+
+    *buffer = newbuf;
+    return realsize;
 }
 
 char *myget(char *url){
