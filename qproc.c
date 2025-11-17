6842d0faea9fadd1a6fd89b8eeb44ffe0eb5517c
Gianluca Mazzini <gianluca@mazzini.org>
2025-11-17 15:43:18 +0100

    Update qproc.c

--- qproc.c
+++ qproc.c
@@ -17,6 +17,8 @@ int main(){
   if(con==NULL)exit(1);
   if(mysql_real_connect(con,dbhost,dbuser,dbpassword,dbname,0,NULL,0)==NULL)exit(1);
   mysql_query(con,"SET time_zone='+00:00'");
+
+  goto next;
   
   printf(">> Insert all new call in the log not just in qrzwc\n");
   sprintf(buf,"select distinct callsign from log where mycall='%s' and callsign not in (select callsign from qrzwebcontact where mycall='%s')",mycall,mycall);
@@ -77,6 +79,7 @@ int main(){
   mysql_free_result(res);
   printf("--- %ld entries with %ld updated\n\n",entry,updated);
 
+  next:
   printf(">> Send email to request xxxx\n");
   sprintf(buf,"select email from who where callsign='%s'",mycall);
   mysql_query(con,buf); res=mysql_store_result(con); row=mysql_fetch_row(res); strcpy(myemail,row[0]); mysql_free_result(res);
@@ -96,7 +99,7 @@ int main(){
     mysql_query(con,buf); res1=mysql_store_result(con); row1=mysql_fetch_row(res1); c=atoi(row1[0]); mysql_free_result(res1);
     sleep(3+rand()%5);
     if(strlen(youremail)>5 && c==0){
-      sprintf(buf,"Hi %s,<br><br> in the past, we have connected and indeed, you are in my log. I noticed that you also have a profile on qrz.com, and I do too. It would really make me happy if you could add your callsign to my qrz.com page called \"Web Contacts,\" where I am collecting a large number of friends. If you decide to proceed, you can: <ul><li>1. log in to the qrz.com website <a href=\"https://www.qrz.com/\"> https://www.qrz.com/</a> with your credentials,</li><li>2. search for my callsign by typing %s or by clicking the link <a href=\"https://www.qrz.com/lookup/%s\">https://www.qrz.com/lookup/$s</a></li><li>3. click on the tab labeled \"Web\",</li><li>4. go to the box labeled \"Add your Web Contact\", and click on the button that says \"DE %s\"</li></ul><br><br>Thank you very much, and I hope to connect with you again soon.<br><br>73 de %s",row[0],mycall,mycall,mycall,mycall,mycall);
+      sprintf(buf,"Hi %s,<br><br> in the past, we have connected and indeed, you are in my log. I noticed that you also have a profile on qrz.com, and I do too. It would really make me happy if you could add your callsign to my qrz.com page called \"Web Contacts,\" where I am collecting a large number of friends. If you decide to proceed, you can: <ul><li>1. log in to the qrz.com website <a href=\"https://www.qrz.com/\"> https://www.qrz.com/</a> with your credentials,</li><li>2. search for my callsign by typing %s or by clicking the link <a href=\"https://www.qrz.com/lookup/%s\">https://www.qrz.com/lookup/%s</a></li><li>3. click on the tab labeled \"Web\",</li><li>4. go to the box labeled \"Add your Web Contact\", and click on the button that says \"DE %s\"</li></ul><br><br>Thank you very much, and I hope to connect with you again soon.<br><br>73 de %s",row[0],mycall,mycall,mycall,row[0],mycall);
       myemailsend(myemail,myemail,"QRZ Web Contacts request",buf);
       sprintf(buf,"update qrzwebcontact set sent=1 where mycall='%s' and callsign='%s'",mycall,row[0]);
       printf("%s\n",buf);
