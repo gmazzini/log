99ff30d4d58626e5ae54277f11b9b620d41fcb45
Gianluca Mazzini <gianluca@mazzini.org>
2025-11-17 15:44:41 +0100

    Update pfunc.c

--- pfunc.c
+++ pfunc.c
@@ -207,7 +207,6 @@ void qrzcom(MYSQL *con,char *call){
   for(n=0;n<13;n++)strcpy(key[n],search(wrbuf,(char *)qrzkey[n]));
   if(key[6][0]!='\0'){
     now=time(NULL); utc=gmtime(&now); strftime(ee,39,"%Y-%m-%d %H:%M:%S",utc);
-    printf("<pre>"); for(n=0;n<13;n++)printf("%s: %s\n",qrzkey[n],key[n]); printf("</pre>");
     sprintf(aux2,"replace into who (callsign,firstname,lastname,addr1,addr2,state,zip,country,grid,email,cqzone,ituzone,born,image,myupdate,src) value ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s',%d,%d,%d,'%s','%s','qrz.com')",call,key[0],key[1],key[2],key[3],key[4],key[5],key[6],key[7],key[8],atoi(key[9]),atoi(key[10]),atoi(key[11]),key[12],ee);
     mysql_query(con,aux2);
   }
@@ -300,7 +299,6 @@ void qrzru(MYSQL *con,char *call){
   for(n=0;n<12;n++)strcpy(key[n],cyrlat(search(wrbuf,(char *)qrzkey[n])));
   if(key[6][0]!='\0'){
     now=time(NULL); utc=gmtime(&now); strftime(ee,39,"%Y-%m-%d %H:%M:%S",utc);
-    printf("<pre>"); for(n=0;n<12;n++)printf("%s: %s\n",qrzkey[n],key[n]); printf("</pre>");
     sprintf(aux2,"replace into who (callsign,firstname,lastname,addr1,addr2,state,zip,country,grid,email,cqzone,ituzone,born,image,myupdate,src) value ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s',%d,%d,%d,'%s','%s','qrz.com')",call,key[0],key[1],key[2],key[3],key[4],key[5],key[6],key[7],"",atoi(key[8]),atoi(key[9]),atoi(key[10]+6),key[11],ee);
     mysql_query(con,aux2);
   }
