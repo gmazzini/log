d524099fffce6f1136cce1eb77fc30ea28b3681f
Gianluca Mazzini <gianluca@mazzini.org>
2025-11-24 05:38:49 +0100

    Update pradio.c

--- pradio.c
+++ pradio.c
@@ -135,12 +135,7 @@ int main(void){
     if(tok[1][0]=='R'){
       sprintf(cmd,"sfim\n");
       write(s,cmd,strlen(cmd));
-      for(loop=i=0;i<100 && loop<20;)if(recv(s,&c,1,MSG_DONTWAIT)==1){b[i++]=c; loop=0;} else {usleep(10000); loop++;} b[i]='\0';
-
-printf("%s\n",b);
-
-      return 1;
-      
+      for(loop=i=0;i<100 && loop<20;)if(recv(s,&c,1,MSG_DONTWAIT)==1){b[i++]=c; loop=0;} else {usleep(10000); loop++;} b[i]='\0';      
       p1=strtok(b,"\n"); p1=strtok(NULL,"\n"); p1=strtok(NULL,"\n");
       printf("%ld,",atol(p1));
       p1=strtok(NULL,"\n"); p1=strtok(NULL,"\n");
