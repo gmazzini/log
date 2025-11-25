2dc7612600b75ceceababc4de37bc31141eba1b0
Gianluca Mazzini <gianluca@mazzini.org>
2025-11-25 04:01:54 +0100

    Update pfunc.c

--- pfunc.c
+++ pfunc.c
@@ -40,13 +40,15 @@ static const uint8_t B64DEC[256] = {
   ['-']=62, ['_']=63
 };
 
-MYSQL_ROW searchcty(MYSQL *con,char *incall){
+char *searchcty(MYSQL *con,char *incall){
   char buf[1000],*p,call[20];
+  static char out[9][50];
   static MYSQL_RES *res;
   static MYSQL_ROW row;
   int i,n;
   const char *suffixes[]={"P","M","LH","MM","AM","A","B","QRP","0","1","2","3","4","5","6","7","8","9"};
   n=sizeof(suffixes)/sizeof(suffixes[0]);
+  for(i=0;i<9;i++)out[i][0]='\0';
   strcpy(call,incall);
   p=strrchr(call,'/');
   if(p){
@@ -69,7 +71,9 @@ MYSQL_ROW searchcty(MYSQL *con,char *incall){
     if(row!=NULL)break;
     mysql_free_result(res);
   }
-  return row;
+  for(i=0;i<9;i++)strcpy(out[i],row[i]);
+  mysql_free_result(res);
+  return out;
 }
 
 long incdata3(int cha,int idx,char *key,long ss,long dd){
