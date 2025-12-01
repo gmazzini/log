1abab4686a4b3a74d7aa08ce87863a26128f3d3f
Gianluca Mazzini <gianluca@mazzini.org>
2025-12-01 07:41:43 +0100

    Update pproc.c

--- pproc.c
+++ pproc.c
@@ -716,8 +716,7 @@ int main(void){
       for(l1=0,idx=0;idx<ndata3[0][0];idx++)l1+=data3[0][0][idx].num;
       for(l2=0,idx=0;idx<ndata3[0][1];idx++)l2+=data3[0][1][idx].num;
       for(l3=0,idx=0;idx<ndata3[0][3];idx++)l3+=data3[0][3][idx].num;
-      f6=(ll3-ll1)/3600.0;
-      printf("%c[ %5.2lf,%ld,%ld,%ld,%ld ]\n",(ll3-ll1>0)?',':' ',f6,l1,l2,l3,l2*l3);
+      printf("%c[ %lld,%ld,%ld,%ld,%ld ]\n",(ll3-ll1>0)?',':' ',ll3-ll1,l1,l2,l3,l2*l3);
     }
     printf("]' style=\"width:100%;height:520px\"></div>");
     goto end;
